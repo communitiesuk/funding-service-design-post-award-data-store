ef7050607ee2d58fceccaf06c10aaf539d94044c
Noel Snape <noel.snape@version1.com>
2023-05-03 12:48:38 +0100

    [#PAB-226] Models for Outcome_Dim and Outcome_Data

--- core/db/entities.py
+++ core/db/entities.py
@@ -2,7 +2,7 @@ import uuid  # noqa
 
 import sqlalchemy as sqla
 
-from core.const import StateEnum
+from core.const import GeographyIndicatorEnum, StateEnum
 from core.db import db
 from core.db.types import GUID
 
@@ -133,18 +133,18 @@ class OutputData(db.Model):
     project_id = sqla.Column(sqla.String, sqla.ForeignKey("project_dim.id"), nullable=False)
     start_date = sqla.Column(sqla.DateTime(), nullable=False)
     end_date = sqla.Column(sqla.DateTime(), nullable=False)
-    output_id = sqla.Column(sqla.ForeignKey("output_dim.id"), nullable=False)
+    output_dim_id = sqla.Column(sqla.ForeignKey("output_dim.id"), nullable=False)
     # TODO: Should this have any extra logic, or is it totally free text?
     #  Also, should it be a field of Outputs_dim instead, or can users map different units of measurement
     #  against the same output?
     unit_of_measurement = sqla.Column(sqla.String, nullable=False)
-    state = sqla.Column(sqla.Enum(StateEnum, name="output_data_status"), nullable=False)
+    state = sqla.Column(sqla.Enum(StateEnum, name="output_data_state"), nullable=False)
     amount = sqla.Column(sqla.Float, nullable=False)
 
     output_dim = sqla.orm.relationship("OutputDim", back_populates="outputs")
 
     # TODO: does this unique index look right?
-    # Unique index for data integrity. There can't be multiple outcomes for a single project with
+    # Unique index for data integrity. There can't be multiple outputs for a single project with
     # the same date range and output metrics.
     __table_args__ = (
         sqla.Index(
@@ -152,7 +152,7 @@ class OutputData(db.Model):
             "project_id",
             "start_date",
             "end_date",
-            "output_id",
+            "output_dim_id",
             "unit_of_measurement",
             "state",
             unique=True,
@@ -177,3 +177,53 @@ class OutputDim(db.Model):
     output_category = sqla.Column(sqla.String, nullable=False, unique=False)
 
     outputs = sqla.orm.relationship("OutputData", back_populates="output_dim")
+
+
+class OutcomeData(db.Model):
+    """Stores Outcome data for projects."""
+
+    __tablename__ = "outcome_data"
+
+    id = sqla.Column(GUID(), default=uuid.uuid4, primary_key=True)  # this should be UUIDType once using Postgres
+    project_id = sqla.Column(sqla.String, sqla.ForeignKey("project_dim.id"), nullable=False)
+    start_date = sqla.Column(sqla.DateTime(), nullable=False)
+    end_date = sqla.Column(sqla.DateTime(), nullable=False)
+    outcome_dim_id = sqla.Column(sqla.ForeignKey("outcome_dim.id"), nullable=False)
+
+    # TODO: as per comment on output
+    unit_of_measurement = sqla.Column(sqla.String, nullable=False)
+    geography_indicator = sqla.Column(sqla.Enum(GeographyIndicatorEnum, name="outcome_data_geography"), nullable=False)
+    amount = sqla.Column(sqla.Float, nullable=False)
+    state = sqla.Column(sqla.Enum(StateEnum, name="outcome_data_state"), nullable=False)
+
+    outcome_dim = sqla.orm.relationship("OutcomeDim", back_populates="outcomes")
+
+    # TODO: does this unique index look right?
+    # Unique index for data integrity. There can't be multiple outcomes for a single project with
+    # the same date range and outcome metrics.
+    __table_args__ = (
+        sqla.Index(
+            "ix_unique_outcome",
+            "project_id",
+            "start_date",
+            "end_date",
+            "outcome_dim_id",
+            "unit_of_measurement",
+            "state",
+            unique=True,
+        ),
+    )
+
+
+# TODO: similar population question as per OutputData
+class OutcomeDim(db.Model):
+    """Stores dimension reference data for Outcomes."""
+
+    __tablename__ = "outcome_dim"
+
+    id = sqla.Column(GUID(), default=uuid.uuid4, primary_key=True)  # this should be UUIDType once using Postgres
+
+    # TODO: Are these a pre-defined finite set? Should they be enum or similar?
+    outcome_category = sqla.Column(sqla.String, nullable=False, unique=False)
+
+    outcomes = sqla.orm.relationship("OutcomeData", back_populates="outcome_dim")
