3dea530169de0fc3e6e8072749cbd478e5aa7f3e
Noel Snape <noel.snape@version1.com>
2023-05-24 14:22:55 +0100

    [#PAB-331] Funding Data

--- core/extraction/round_two.py
+++ core/extraction/round_two.py
@@ -25,6 +25,11 @@ def ingest_round_two_data(df_ingest: pd.DataFrame) -> Dict[str, pd.DataFrame]:
     extracted_data["df_project_progress_extracted"] = extract_project_progress(df_ingest)
     extracted_data["df_funding_questions_extracted"] = extract_funding_questions(df_ingest)
 
+    # Funding DataFrame very large - needs logic to split into DM separate rows
+    extracted_data["df_funding_extracted"] = extract_funding_data(df_ingest)
+
+    # Note: No data fields for funding comments in Round 2 data-set
+
     return extracted_data
 
 
@@ -113,6 +118,30 @@ def extract_funding_questions(df_input: pd.DataFrame) -> pd.DataFrame:
     )
 
     return df_funding_questions
+
+
+def extract_funding_data(df_input: pd.DataFrame) -> pd.DataFrame:
+    """
+    Extract funding data from DataFrame.
+
+    Input dataframe is parsed from Excel spreadsheet: "Round 2 Reporting - Consolidation".
+
+    :param df_input: Input DataFrame containing consolidated data.
+    :return: A new DataFrame containing the extracted funding data.
+    """
+    # TODO: what does the section between columns
+    #  "Tab 4 - Funding Profiles B2 CDEL Prog Mngmt - Pre-20/21 H1 - CDEL Prog Mngmt" and
+    #  "Tab 4 - Funding Profiles B2  Prog Mngmt Totals - Grand Total - Prog Mngmt Totals"
+    #  translate to? There seems to be no matching data in the TF form?
+
+    index_1 = "Tab 4 - Funding Profiles C 1st Row - Pre-20/21 H1 - CDEL Utilised"
+    index_2 = "Tab 4 - Funding Profiles C 8th Row - Grand Total - TF Funding Total"
+    df_funding = df_input.loc[:, index_1:index_2]
+    index_1 = "Tab 4 - Funding Profiles Other Funding Sources 1 -  Funding Source - OFS 1"
+    index_2 = "Tab 4 - Funding Profiles Other Funding Sources 28 - Grand Total - OFS 5"
+    df_funding = pd.concat([df_funding, df_input.loc[:, index_1:index_2]], axis=1)
+    df_funding = join_to_project(df_input, df_funding)
+    return df_funding
 
 
 # assuming this slice of 3 cols is the most suitable to identify programme by
