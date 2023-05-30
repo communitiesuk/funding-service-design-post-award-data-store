dc70e45a19df4b340ef851b569a3669d01c1d4cc
Gideon Goldberg <1764158+gidsg@users.noreply.github.com>
2023-05-30 08:49:23 +0100

    BAU: Use absolute db file path (#50)
    * use absolute path to persisted file
    
    * use system temp file path

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import tempfile
 from copy import deepcopy
 from pathlib import Path
 
@@ -35,8 +36,9 @@ def create_app() -> Flask:
     logging.init_app(flask_app)
 
     flask_app.config["SCHEMAS"] = {"towns_fund": parse_schema(deepcopy(TF_SCHEMA))}
+    db_file_path = f"sqlite:///{tempfile.gettempdir()}/sqlite.db"
     flask_app.config["SQLALCHEMY_DATABASE_URI"] = (
-        "sqlite:///temp.db" if "PERSIST_DB" in os.environ else "sqlite:///:memory:"
+        db_file_path if "PERSIST_DB" in os.environ else "sqlite:///:memory:"
     )  # disk-based db persists and allows for multiple connections
     db.init_app(flask_app)
 
