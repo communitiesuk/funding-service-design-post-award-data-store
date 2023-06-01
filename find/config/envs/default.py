111614cdabb1bd5332d81e566b88a23e172cb1b3
Gideon Goldberg <1764158+gidsg@users.noreply.github.com>
2023-06-01 17:47:25 +0100

    default config values
    Set values in default config rather than .flaskenv to remove discrepencies between environments

--- find/config/envs/default.py
+++ find/config/envs/default.py
@@ -9,12 +9,19 @@ from fsd_utils import configclass
 class DefaultConfig(object):
     FLASK_ROOT = str(Path(__file__).parent.parent.parent)
 
-    CONTACT_EMAIL = os.environ.get("CONTACT_EMAIL", "dev@dev.com")
+    CONTACT_EMAIL = os.environ.get("CONTACT_EMAIL", "FSD.Support@levellingup.gov.uk")
     CONTACT_PHONE = os.environ.get("CONTACT_PHONE", "12345678910")
-    DEPARTMENT_NAME = os.environ.get("DEPARTMENT_NAME", "dev-dept-name")
-    DEPARTMENT_URL = os.environ.get("DEPARTMENT_URL", "dev-dept-url")
-    SERVICE_NAME = os.environ.get("SERVICE_NAME", "post-award-frontend")
-    SERVICE_PHASE = os.environ.get("SERVICE_PHASE", "dev-service-phase")
+    DEPARTMENT_NAME = os.environ.get(
+        "DEPARTMENT_NAME", "Department for Levelling Up, Housing and Communities"
+    )
+    DEPARTMENT_URL = os.environ.get(
+        "DEPARTMENT_URL",
+        "https://www.gov.uk/government/organisations/department-for-levelling-up-housing-and-communities",
+    )
+    SERVICE_NAME = os.environ.get(
+        "SERVICE_NAME", "Download monitoring and evaluation data"
+    )
+    SERVICE_PHASE = os.environ.get("SERVICE_PHASE", "BETA")
     SERVICE_URL = os.environ.get("SERVICE_URL", "dev-service-url")
     SESSION_COOKIE_SECURE = True
     DATA_STORE_API_HOST = os.environ.get("DATA_STORE_API_HOST")
