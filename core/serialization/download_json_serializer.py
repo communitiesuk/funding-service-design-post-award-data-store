29283bb50c1fed309d6c2e86d31ae2822ed3d440
Cyrus <22678716+cyrusdobbs@users.noreply.github.com>
2023-06-05 12:31:12 +0100

    [#PAB-429] create json serializers and db query functions

---
+++ core/serialization/download_json_serializer.py
@@ -0,0 +1,316 @@
+import core.db.entities as entities
+from core.db.entities import OutcomeDim, OutputDim
+
+
+def serialize_download_data(organisations, programmes, projects, outcomes):
+    """Top level serialization of the download data."""
+
+    # Organisation level data
+    organisation_refs = [OrganisationSerializer(organisation).to_json() for organisation in organisations]
+
+    # Programme level data
+    programme_progresses = [
+        ProgrammeProgressSerializer(prog_progress).to_json()
+        for programme in programmes
+        for prog_progress in programme.progress_records
+    ]
+    programme_refs = [ProgrammeSerializer(prog).to_json() for prog in programmes]
+    place_details = [
+        PlaceDetailSerializer(place_detail).to_json()
+        for programme in programmes
+        for place_detail in programme.place_details
+    ]
+    funding_questions = [
+        FundingQuestionSerializer(funding_q).to_json()
+        for programme in programmes
+        for funding_q in programme.funding_questions
+    ]
+    programme_risks = [RiskRegisterSerializer(risk).to_json() for programme in programmes for risk in programme.risks]
+
+    # Project level data
+    project_details = [ProjectSerializer(proj).to_json() for proj in projects]
+    project_progresses = [
+        ProjectProgressSerializer(proj_prog).to_json() for project in projects for proj_prog in project.progress_records
+    ]
+    funding = [FundingSerializer(funding).to_json() for project in projects for funding in project.funding_records]
+    funding_comments = [
+        FundingCommentSerializer(funding_comment).to_json()
+        for project in projects
+        for funding_comment in project.funding_comments
+    ]
+    private_investments = [
+        PrivateInvestmentSerializer(private_investment).to_json()
+        for project in projects
+        for private_investment in project.private_investments
+    ]
+    outputs_ref = [OutputDimSerializer(output_dim).to_json() for output_dim in OutputDim.query.all()]
+    output_data = [
+        OutputDataSerializer(output_data).to_json() for project in projects for output_data in project.outputs
+    ]
+    outcome_ref = [OutcomeDimSerializer(outcome_dim).to_json() for outcome_dim in OutcomeDim.query.all()]
+    outcome_data = [OutcomeDataSerializer(outcome_data).to_json() for outcome_data in outcomes]
+    project_risks = [RiskRegisterSerializer(risk).to_json() for project in projects for risk in project.risks]
+    risks = [*programme_risks, *project_risks]  # risks are combination of programme and project risks
+
+    # Provides sheet order for when outputted in Excel format.
+    download_data = {
+        "PlaceDetails": place_details,
+        "ProjectDetails": project_details,
+        "OrganisationRef": organisation_refs,
+        "ProgrammeRef": programme_refs,
+        "ProgrammeProgress": programme_progresses,
+        "ProjectProgress": project_progresses,
+        "FundingQuestions": funding_questions,
+        "Funding": funding,
+        "FundingComments": funding_comments,
+        "PrivateInvestments": private_investments,
+        "OutputsRef": outputs_ref,
+        "OutputData": output_data,
+        "OutcomeRef": outcome_ref,
+        "OutcomeData": outcome_data,
+        "RiskRegister": risks,
+    }
+    return download_data
+
+
+class OrganisationSerializer:
+    def __init__(self, organisation: entities.Organisation):
+        self.organisation = organisation
+
+    def to_json(self):
+        return {
+            "OrganisationID": str(self.organisation.id),
+            "OrganisationName": self.organisation.organisation_name,
+            "Geography": self.organisation.geography,
+        }
+
+
+class ProgrammeSerializer:
+    def __init__(self, programme: entities.Programme):
+        self.programme = programme
+
+    def to_json(self):
+        return {
+            "ProgrammeID": self.programme.programme_id,
+            "ProgrammeName": self.programme.programme_name,
+            "FundTypeID": self.programme.fund_type_id,
+            "OrganisationID": str(self.programme.organisation_id),
+        }
+
+
+class ProgrammeProgressSerializer:
+    def __init__(self, programme_progress: entities.ProgrammeProgress):
+        self.programme_progress = programme_progress
+
+    def to_json(self):
+        return {
+            "ID": str(self.programme_progress.id),
+            "SubmissionID": self.programme_progress.submission.submission_id,
+            "ProgrammeID": self.programme_progress.programme.programme_id,
+            "Question": self.programme_progress.question,
+            "Answer": self.programme_progress.answer,
+        }
+
+
+class PlaceDetailSerializer:
+    def __init__(self, place_detail: entities.PlaceDetail):
+        self.place_detail = place_detail
+
+    def to_json(self):
+        return {
+            "ID": str(self.place_detail.id),
+            "SubmissionID": self.place_detail.submission.submission_id,
+            "Question": self.place_detail.question,
+            "Answer": self.place_detail.answer,
+            "ProgrammeID": self.place_detail.programme.programme_id,
+            "Indicator": self.place_detail.indicator,
+        }
+
+
+class FundingQuestionSerializer:
+    def __init__(self, funding_question: entities.FundingQuestion):
+        self.funding_question = funding_question
+
+    def to_json(self):
+        return {
+            "ID": str(self.funding_question.id),
+            "SubmissionID": self.funding_question.submission.submission_id,
+            "ProgrammeID": self.funding_question.programme.programme_id,
+            "Question": self.funding_question.question,
+            "Indicator": self.funding_question.indicator,
+            "Response": self.funding_question.response,
+            "GuidanceNotes": self.funding_question.guidance_notes,
+        }
+
+
+class ProjectSerializer:
+    def __init__(self, project: entities.Project):
+        self.project = project
+
+    def to_json(self):
+        return {
+            "ID": str(self.project.id),
+            "SubmissionID": self.project.submission.submission_id,
+            "ProjectID": self.project.project_id,
+            "ProjectName": self.project.project_name,
+            "PrimaryInterventionTheme": self.project.primary_intervention_theme,
+            "SingleorMultipleLocations": self.project.location_multiplicity,
+            "Locations": self.project.locations,
+        }
+
+
+class ProjectProgressSerializer:
+    def __init__(self, project_progress: entities.ProjectProgress):
+        self.project_progress = project_progress
+
+    def to_json(self):
+        return {
+            "ID": str(self.project_progress.id),
+            "ProjectID": self.project_progress.project.project_id,
+            "SubmissionID": self.project_progress.submission.submission_id,
+            "StartDate": str(self.project_progress.start_date),
+            "CompletionDate": str(self.project_progress.end_date),
+            "ProjectAdjustmentRequestStatus": self.project_progress.adjustment_request_status,
+            "ProjectDeliveryStatus": self.project_progress.delivery_status,
+            "Delivery(RAG)": self.project_progress.delivery_rag,
+            "Spend(RAG)": self.project_progress.spend_rag,
+            "Risk(RAG)": self.project_progress.risk_rag,
+            "CommentaryonStatusandRAGRatings": self.project_progress.commentary,
+            "MostImportantUpcomingCommsMilestone": self.project_progress.important_milestone,
+            "DateofMostImportantUpcomingCommsMilestone(e.g.Dec-22)": self.project_progress.date_of_important_milestone,
+        }
+
+
+class FundingSerializer:
+    def __init__(self, funding: entities.Funding):
+        self.funding = funding
+
+    def to_json(self):
+        return {
+            "ID": str(self.funding.id),
+            "SubmissionID": self.funding.submission.submission_id,
+            "ProjectID": self.funding.project.project_id,
+            "FundingSourceName": self.funding.funding_source_name,
+            "FundingSourceType": self.funding.funding_source_type,
+            "Secured": self.funding.secured,
+            "ReportingPeriod": self.funding.reporting_period,
+            "SpendforReportingPeriod": self.funding.spend_for_reporting_period,
+            "Actual": self.funding.status,
+        }
+
+
+class FundingCommentSerializer:
+    def __init__(self, funding_comment: entities.FundingComment):
+        self.funding_comment = funding_comment
+
+    def to_json(self):
+        return {
+            "ID": str(self.funding_comment.id),
+            "SubmissionID": self.funding_comment.submission.submission_id,
+            "ProjectID": self.funding_comment.project.project_id,
+            "Comment": self.funding_comment.comment,
+        }
+
+
+class PrivateInvestmentSerializer:
+    def __init__(self, private_investment: entities.PrivateInvestment):
+        self.private_investment = private_investment
+
+    def to_json(self):
+        return {
+            "ID": str(self.private_investment.id),
+            "SubmissionID": self.private_investment.submission.submission_id,
+            "ProjectID": self.private_investment.project.project_id,
+            "TotalProjectValue": self.private_investment.total_project_value,
+            "TownsfundFunding": self.private_investment.townsfund_funding,
+            "PrivateSectorFundingRequired": self.private_investment.private_sector_funding_required,
+            "PrivateSectorFundingSecured": self.private_investment.private_sector_funding_secured,
+            "AdditionalComments": self.private_investment.additional_comments,
+        }
+
+
+class OutputDataSerializer:
+    def __init__(self, output_data: entities.OutputData):
+        self.output_data = output_data
+
+    def to_json(self):
+        return {
+            "ID": str(self.output_data.id),
+            "SubmissionID": self.output_data.submission.submission_id,
+            "ProjectID": self.output_data.project.project_id,
+            "Start_Date": str(self.output_data.start_date),
+            "End_Date": str(self.output_data.end_date),
+            "Output": self.output_data.output_dim.output_name,
+            "UnitofMeasurement": self.output_data.unit_of_measurement,
+            "Actual": self.output_data.state,
+            "Amount": self.output_data.amount,
+            "AdditionalInformation": self.output_data.additional_information,
+        }
+
+
+class OutputDimSerializer:
+    def __init__(self, output_dim: entities.OutputDim):
+        self.output_dim = output_dim
+
+    def to_json(self):
+        return {
+            "ID": str(self.output_dim.id),
+            "OutputName": self.output_dim.output_name,
+            "OutputCategory": self.output_dim.output_category,
+        }
+
+
+class OutcomeDataSerializer:
+    def __init__(self, outcome_data: entities.OutcomeData):
+        self.outcome_data = outcome_data
+
+    def to_json(self):
+        return {
+            "ID": str(self.outcome_data.id),
+            "SubmissionID": self.outcome_data.submission.submission_id,
+            "ProjectID": self.outcome_data.project.project_id,
+            "Start_Date": str(self.outcome_data.start_date),
+            "End_Date": str(self.outcome_data.end_date),
+            "Outcome": self.outcome_data.outcome_dim.outcome_name,
+            "UnitofMeasurement": self.outcome_data.unit_of_measurement,
+            "GeographyIndicator": self.outcome_data.geography_indicator,
+            "Amount": self.outcome_data.amount,
+            "Actual": self.outcome_data.state,
+        }
+
+
+class OutcomeDimSerializer:
+    def __init__(self, outcome_dim: entities.OutcomeDim):
+        self.outcome_dim = outcome_dim
+
+    def to_json(self):
+        return {
+            "ID": str(self.outcome_dim.id),
+            "Outcome_Name": self.outcome_dim.outcome_name,
+            "Outcome_Category": self.outcome_dim.outcome_category,
+        }
+
+
+class RiskRegisterSerializer:
+    def __init__(self, risk_register: entities.RiskRegister):
+        self.risk_register = risk_register
+
+    def to_json(self):
+        return {
+            "ID": str(self.risk_register.id),
+            "SubmissionID": self.risk_register.submission.submission_id,
+            "ProgrammeID": self.risk_register.programme.programme_id if self.risk_register.programme else None,
+            "ProjectID": self.risk_register.project.project_id if self.risk_register.project else None,
+            "RiskName": self.risk_register.risk_name,
+            "RiskCategory": self.risk_register.risk_category,
+            "ShortDescription": self.risk_register.short_desc,
+            "FullDescription": self.risk_register.full_desc,
+            "Consequences": self.risk_register.consequences,
+            "Pre-mitigatedImpact": self.risk_register.pre_mitigated_impact,
+            "Pre-mitigatedLikelihood": self.risk_register.pre_mitigated_likelihood,
+            "Mitigations": self.risk_register.mitigations,
+            "PostMitigatedImpact": self.risk_register.post_mitigated_impact,
+            "PostMitigatedLikelihood": self.risk_register.post_mitigated_likelihood,
+            "Proximity": self.risk_register.proximity,
+            "RiskOwnerRole": self.risk_register.risk_owner_role,
+        }
---
+++ tests/db_tests/test_queries.py
@@ -0,0 +1,396 @@
+from datetime import datetime
+
+import pytest
+
+from core.const import GeographyIndicatorEnum, MultiplicityEnum
+from core.db import db
+
+# isort: off
+from core.db.entities import Organisation, OutcomeData, OutcomeDim, Programme, Project, Submission
+
+# isort: on
+
+
+@pytest.fixture
+def submissions(app_ctx):
+    # Create some sample submissions for testing
+    sub1 = Submission(
+        submission_id="1",
+        submission_date=datetime(2023, 5, 1),
+        reporting_period_start=datetime(2023, 4, 1),
+        reporting_period_end=datetime(2023, 4, 30),
+        reporting_round=1,
+    )
+    sub2 = Submission(
+        submission_id="2",
+        submission_date=datetime(2023, 5, 5),
+        reporting_period_start=datetime(2023, 5, 1),
+        reporting_period_end=datetime(2023, 5, 31),
+        reporting_round=2,
+    )
+    sub3 = Submission(
+        submission_id="3",
+        submission_date=datetime(2023, 6, 1),
+        reporting_period_start=datetime(2023, 6, 1),
+        reporting_period_end=datetime(2023, 6, 30),
+        reporting_round=1,
+    )
+    sub4 = Submission(
+        submission_id="4",
+        submission_date=datetime(2023, 6, 5),
+        reporting_period_start=datetime(2023, 6, 1),
+        reporting_period_end=datetime(2023, 6, 30),
+        reporting_round=2,
+    )
+    submissions = [sub1, sub2, sub3, sub4]
+    db.session.add_all(submissions)
+    db.session.flush()
+    return submissions
+
+
+@pytest.fixture
+def sub_ids(app_ctx, submissions):
+    sub_ids = [
+        Submission.query.filter_by(submission_id=submissions[idx].submission_id).first().id
+        for idx in range(len(submissions))
+    ]
+    return sub_ids
+
+
+def test_get_submissions_by_reporting_period_all(submissions):
+    result = Submission.get_submissions_by_reporting_period(None, None)
+
+    assert len(result) == len(submissions)
+    assert all(sub in result for sub in submissions)
+
+
+def test_get_submissions_by_reporting_period_with_start_and_end(submissions):
+    start = datetime(2023, 3, 1)
+    end = datetime(2023, 5, 31)
+
+    result = Submission.get_submissions_by_reporting_period(start, end)
+
+    assert len(result) == 2
+    assert all(
+        start <= sub.reporting_period_start <= end and start <= sub.reporting_period_end <= end for sub in result
+    )
+
+
+def test_get_submissions_by_reporting_period_with_start(submissions):
+    start = datetime(2023, 6, 1)
+
+    result = Submission.get_submissions_by_reporting_period(start, None)
+
+    assert len(result) == 2
+    assert all(start <= sub.reporting_period_start for sub in result)
+
+
+def test_get_submissions_by_reporting_period_with_end(submissions):
+    end = datetime(2023, 6, 30)
+
+    result = Submission.get_submissions_by_reporting_period(None, end)
+
+    assert len(result) == len(submissions)
+    assert all(sub.reporting_period_end <= end for sub in result)
+
+
+@pytest.fixture
+def organisations(app_ctx):
+    # Create some sample organisations for testing
+    org1 = Organisation(organisation_name="Org 1", geography="Geography 1")
+    org2 = Organisation(organisation_name="Org 2", geography="Geography 2")
+    org3 = Organisation(organisation_name="Org 3", geography="Geography 3")
+    org4 = Organisation(organisation_name="Org 4", geography="Geography 4")
+    orgs = [org1, org2, org3, org4]
+    db.session.add_all(orgs)
+    db.session.flush()
+    return orgs
+
+
+@pytest.fixture
+def org_ids(app_ctx, organisations):
+    org_ids = [
+        Organisation.query.filter_by(organisation_name=organisations[idx].organisation_name).first().id
+        for idx in range(len(organisations))
+    ]
+    return org_ids
+
+
+def test_get_organisations_by_name_with_names(organisations):
+    # Arrange
+    names = ["Org 2", "Org 4"]
+
+    # Act
+    result = Organisation.get_organisations_by_name(names)
+
+    # Assert
+    assert len(result) == 2
+    assert all(org.organisation_name in names for org in result)
+
+
+def test_get_organisations_by_name_without_names(organisations):
+    result = Organisation.get_organisations_by_name([])
+
+    assert len(result) == len(organisations)
+    assert all(org in result for org in organisations)
+
+
+@pytest.fixture
+def programmes(app_ctx, org_ids):
+    # Create some sample programmes for testing
+    prog1 = Programme(
+        programme_id="1", programme_name="Programme 1", fund_type_id="fund_type_1", organisation_id=org_ids[0]
+    )
+    prog2 = Programme(
+        programme_id="2", programme_name="Programme 2", fund_type_id="fund_type_1", organisation_id=org_ids[1]
+    )
+    prog3 = Programme(
+        programme_id="3", programme_name="Programme 3", fund_type_id="fund_type_2", organisation_id=org_ids[0]
+    )
+    prog4 = Programme(
+        programme_id="4", programme_name="Programme 4", fund_type_id="fund_type_2", organisation_id=org_ids[2]
+    )
+    programmes = [prog1, prog2, prog3, prog4]
+    db.session.add_all(programmes)
+    db.session.flush()
+    return programmes
+
+
+@pytest.fixture
+def prog_ids(app_ctx, programmes):
+    prog_ids = [
+        Programme.query.filter_by(programme_name=programmes[idx].programme_name).first().id
+        for idx in range(len(programmes))
+    ]
+    return prog_ids
+
+
+def test_get_programmes_by_org_and_fund_type_all(programmes):
+    result = Programme.get_programmes_by_org_and_fund_type([], [])
+
+    assert len(result) == len(programmes)
+    assert all(prog in result for prog in programmes)
+
+
+def test_get_programmes_by_org_and_fund_type_with_org_and_fund_type(programmes, org_ids):
+    query_org_ids = [org_ids[0], org_ids[1]]
+    fund_type_ids = ["fund_type_1"]
+
+    result = Programme.get_programmes_by_org_and_fund_type(query_org_ids, fund_type_ids)
+
+    assert len(result) == 2
+    assert all(prog.organisation_id in query_org_ids and prog.fund_type_id in fund_type_ids for prog in result)
+
+
+def test_get_programmes_by_org_and_fund_type_with_org_only(programmes, org_ids):
+    query_org_ids = [org_ids[2]]
+
+    result = Programme.get_programmes_by_org_and_fund_type(query_org_ids, [])
+
+    assert len(result) == 1
+    assert all(prog.organisation_id in query_org_ids for prog in result)
+
+
+def test_get_programmes_by_org_and_fund_type_with_fund_type_only(programmes):
+    fund_type_ids = ["fund_type_2"]
+
+    result = Programme.get_programmes_by_org_and_fund_type([], fund_type_ids)
+
+    assert len(result) == 2
+    assert all(prog.fund_type_id in fund_type_ids for prog in result)
+
+
+@pytest.fixture
+def projects(app_ctx, prog_ids, sub_ids):
+    # Create some sample projects for testing
+    proj1 = Project(
+        project_id="1",
+        submission_id=sub_ids[0],
+        programme_id=prog_ids[0],
+        project_name="Project 1",
+        primary_intervention_theme="Theme 1",
+        location_multiplicity=MultiplicityEnum.MULTIPLE,
+        locations="Location 1",
+        gis_provided="Yes",
+        lat_long="12345",
+    )
+    proj2 = Project(
+        project_id="2",
+        submission_id=sub_ids[0],
+        programme_id=prog_ids[1],
+        project_name="Project 2",
+        primary_intervention_theme="Theme 2",
+        location_multiplicity=MultiplicityEnum.MULTIPLE,
+        locations="Location 2",
+        gis_provided="No",
+        lat_long=None,
+    )
+    proj3 = Project(
+        project_id="3",
+        submission_id=sub_ids[1],
+        programme_id=prog_ids[0],
+        project_name="Project 3",
+        primary_intervention_theme="Theme 1",
+        location_multiplicity=MultiplicityEnum.MULTIPLE,
+        locations="Location 3",
+        gis_provided="Yes",
+        lat_long="67890",
+    )
+    proj4 = Project(
+        project_id="4",
+        submission_id=sub_ids[1],
+        programme_id=prog_ids[1],
+        project_name="Project 4",
+        primary_intervention_theme="Theme 2",
+        location_multiplicity=MultiplicityEnum.MULTIPLE,
+        locations="Location 4",
+        gis_provided="No",
+        lat_long=None,
+    )
+    projects = [proj1, proj2, proj3, proj4]
+    db.session.add_all(projects)
+    db.session.flush()
+    return projects
+
+
+@pytest.fixture
+def proj_ids(app_ctx, projects):
+    proj_ids = [
+        Project.query.filter_by(project_name=projects[idx].project_name).first().id for idx in range(len(projects))
+    ]
+    return proj_ids
+
+
+def test_get_project_by_programme_ids_and_submission_ids_none(app_ctx):
+    result = Project.get_project_by_programme_ids_and_submission_ids([], [])
+
+    assert len(result) == 0
+
+
+def test_get_project_by_programme_ids_and_submission_ids_with_programme_and_submission(projects, prog_ids, sub_ids):
+    programme_ids = [prog_ids[0], prog_ids[1]]
+    submission_ids = [sub_ids[0]]
+
+    result = Project.get_project_by_programme_ids_and_submission_ids(programme_ids, submission_ids)
+
+    assert len(result) == 2
+    assert all(proj.programme_id in programme_ids and proj.submission_id in submission_ids for proj in result)
+
+
+def test_get_project_by_programme_ids_and_submission_ids_with_programme_only(projects, prog_ids):
+    programme_ids = [prog_ids[1]]
+
+    result = Project.get_project_by_programme_ids_and_submission_ids(programme_ids, [])
+
+    assert len(result) == 0
+    assert all(proj.programme_id in programme_ids for proj in result)
+
+
+def test_get_project_by_programme_ids_and_submission_ids_with_submission_only(projects, sub_ids):
+    submission_ids = [sub_ids[1]]
+
+    result = Project.get_project_by_programme_ids_and_submission_ids([], submission_ids)
+
+    assert len(result) == 0
+    assert all(proj.submission_id in submission_ids for proj in result)
+
+
+@pytest.fixture
+def outcomes(sub_ids, prog_ids, proj_ids):
+    # Create some sample outcomes for testing
+    outcome_dim1 = OutcomeDim(outcome_name="OutcomeDim 1", outcome_category="Category 1")
+    outcome_dim2 = OutcomeDim(outcome_name="OutcomeDim 2", outcome_category="Category 2")
+    outcome_dims = [outcome_dim1, outcome_dim2]
+    db.session.add_all(outcome_dims)
+    db.session.flush()
+    outcome_dim_ids = [
+        OutcomeDim.query.filter_by(outcome_name=outcome_dim.outcome_name).first().id for outcome_dim in outcome_dims
+    ]
+
+    outcome1 = OutcomeData(
+        submission_id=sub_ids[0],
+        project_id=proj_ids[0],
+        outcome_id=outcome_dim_ids[0],  # Category 1
+        start_date=datetime(2022, 1, 1),
+        end_date=datetime(2022, 12, 31),
+        unit_of_measurement="Units",
+        geography_indicator=GeographyIndicatorEnum.TRAVEL_CORRIDOR,
+        amount=100.0,
+        state="Actual",
+        higher_frequency=None,
+    )
+    outcome2 = OutcomeData(
+        submission_id=sub_ids[0],
+        project_id=proj_ids[1],
+        outcome_id=outcome_dim_ids[0],  # Category 1
+        start_date=datetime(2022, 1, 1),
+        end_date=datetime(2022, 12, 31),
+        unit_of_measurement="Units",
+        geography_indicator=GeographyIndicatorEnum.TRAVEL_CORRIDOR,
+        amount=200.0,
+        state="Actual",
+        higher_frequency=None,
+    )
+    outcome3 = OutcomeData(
+        submission_id=sub_ids[0],
+        project_id=proj_ids[0],
+        outcome_id=outcome_dim_ids[1],  # Category 2
+        start_date=datetime(2022, 1, 1),
+        end_date=datetime(2022, 12, 31),
+        unit_of_measurement="Units",
+        geography_indicator=GeographyIndicatorEnum.TRAVEL_CORRIDOR,
+        amount=300.0,
+        state="Actual",
+        higher_frequency=None,
+    )
+    outcome4 = OutcomeData(
+        submission_id=sub_ids[0],
+        programme_id=prog_ids[0],
+        outcome_id=outcome_dim_ids[1],  # Category 2
+        start_date=datetime(2022, 1, 1),
+        end_date=datetime(2022, 12, 31),
+        unit_of_measurement="Units",
+        geography_indicator=GeographyIndicatorEnum.TRAVEL_CORRIDOR,
+        amount=400.0,
+        state="Actual",
+        higher_frequency=None,
+    )
+    outcomes = [outcome1, outcome2, outcome3, outcome4]
+    db.session.add_all(outcomes)
+    db.session.flush()
+    return outcomes
+
+
+def test_get_outcomes_by_project_ids_and_categories_none(outcomes):
+    result = OutcomeData.get_outcomes_by_project_ids_and_categories([], [])
+
+    assert len(result) == 0
+
+
+def test_get_outcomes_by_project_ids_and_categories_with_project_and_category(outcomes, proj_ids):
+    project_ids = [proj_ids[0]]
+    categories = ["Category 1"]
+
+    result = OutcomeData.get_outcomes_by_project_ids_and_categories(project_ids, categories)
+
+    assert len(result) == 1
+    assert all(
+        outcome.project_id in project_ids and outcome.outcome_dim.outcome_category in categories for outcome in result
+    )
+
+
+def test_get_outcomes_by_project_ids_and_categories_with_project_only(outcomes, proj_ids):
+    project_ids = [proj_ids[0]]
+
+    result = OutcomeData.get_outcomes_by_project_ids_and_categories(project_ids, [])
+
+    assert len(result) == 2
+    assert all(outcome.project_id in project_ids for outcome in result)
+
+
+def test_get_outcomes_by_project_ids_and_categories_with_category_only(outcomes):
+    categories = ["Category 2"]
+
+    result = OutcomeData.get_outcomes_by_project_ids_and_categories([], categories)
+
+    assert len(result) == 0
