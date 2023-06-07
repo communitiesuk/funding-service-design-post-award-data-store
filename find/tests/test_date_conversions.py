85206ef4ca2eadf518fff74b919abd230bc0cc11
Adrian <adrian.elson@version1.com>
2023-06-07 14:19:15 +0100

    added edge case to test

--- find/tests/test_date_conversions.py
+++ find/tests/test_date_conversions.py
@@ -1,8 +1,15 @@
 from app.main.download_data import generate_financial_years
+import pytest
 
 
-def test_convert_iso_datetime_ranges_to_financial_year_periods():
+# TODO: Create test: test_populate_financial_years()
+def test_convert_to_periods():
     min_date_str = "2019-05-01"
     max_date_str = "2023-06-01"
     output = (generate_financial_years(min_date_str, max_date_str))
     assert output == ['2019/2020', '2020/2021', '2021/2022', '2022/2023', '2023/2024']
+
+    min_date_str = "2019-03-31"
+    max_date_str = "2023-03-31"
+    output = (generate_financial_years(min_date_str, max_date_str))
+    assert output == ['2018/2019', '2019/2020', '2020/2021', '2021/2022', '2022/2023']
