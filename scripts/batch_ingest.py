674290fef99ada07687e9cda9ee278ee2b3b9cf4
Cyrus <22678716+cyrusdobbs@users.noreply.github.com>
2023-06-13 12:06:09 +0100

    [BAU] add export results to excel option to batch ingest script

--- scripts/batch_ingest.py
+++ scripts/batch_ingest.py
@@ -1,7 +1,12 @@
 import argparse
 import os
+from datetime import datetime
 
+import pandas as pd
 import requests
+from openpyxl.styles import Alignment
+from openpyxl.utils.dataframe import dataframe_to_rows
+from openpyxl.workbook import Workbook
 
 
 def post_file(file_path, endpoint_url):
@@ -26,42 +31,94 @@ def save_response(file_path, response, output_folder):
         file.write(response.text)
 
 
+def output_to_excel():
+    output_df = pd.DataFrame(columns=["File Name", "Success", "Errors"])
+
+    dir_items = os.listdir(args.directory_path)
+    for idx, file_name in enumerate(dir_items):
+        if file_name == "output":
+            continue
+
+        print(
+            f"Testing file {idx}/{len(dir_items) - 1} - {file_name}. \nSuccess ({output_df['Success'].sum()}), "
+            f"Failed ({len(output_df) - output_df['Success'].sum()})"
+        )
+
+        file_path = os.path.join(args.directory_path, file_name)
+        if os.path.isfile(file_path):
+            response = post_file(file_path, args.endpoint_url)
+
+            status_code = response.status_code
+            success = status_code == requests.codes.ok
+            if success:
+                errors = ""
+            elif status_code == 400:  # validation error
+                errors = "\n".join(response.json()["validation_errors"])
+            else:  # internal error e.g. DB error
+                errors = response.text
+
+            output_df.loc[len(output_df)] = {"File Name": file_name, "Success": success, "Errors": errors}
+            print("Success." if success else "Failed.")
+        print()
+
+    workbook = Workbook()
+    sheet = workbook.active
+    sheet.column_dimensions["C"].width = 160
+    for row in dataframe_to_rows(output_df, index=False, header=True):
+        sheet.append(row)
+    for row in sheet.iter_rows(min_row=2, max_row=sheet.max_row, min_col=1, max_col=sheet.max_column):
+        row[2].alignment = Alignment(wrap_text=True)
+    workbook.save(output_folder + f"/results_{datetime.now().strftime('%Y%m%dT%H%M%S%z')}.xlsx")
+
+
+def output_to_directories():
+    success_responses = []
+    failure_responses = []
+
+    dir_items = os.listdir(args.directory_path)
+    for idx, file_name in enumerate(dir_items):
+        if file_name == "output":
+            continue
+
+        print(
+            f"Testing file {idx}/{len(dir_items)} - {file_name}. \nSuccess ({len(success_responses)}), "
+            f"Failed ({len(failure_responses)})"
+        )
+
+        file_path = os.path.join(args.directory_path, file_name)
+        if os.path.isfile(file_path):
+            response = post_file(file_path, args.endpoint_url)
+
+            if response.status_code == requests.codes.ok:
+                success_responses.append(file_name)
+                print("Success.")
+            else:
+                failure_responses.append(file_name)
+                save_response(file_path, response, output_folder)
+                print("Failed.")
+        print()
+
+    with open(os.path.join(output_folder, "success_responses.txt"), "w") as success_file:
+        success_file.write("\n\n".join(success_responses))
+
+    with open(os.path.join(output_folder, "failure_responses.txt"), "w") as failure_file:
+        failure_file.write("\n\n".join(failure_responses))
+
+
 parser = argparse.ArgumentParser(description="Process files and post to an endpoint.")
 parser.add_argument("directory_path", type=str, help="Path to the directory containing the files.")
 parser.add_argument("endpoint_url", type=str, help="URL of the endpoint to post the files to.")
+
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument("-d", "--directories", action="store_true", help="Store results to separate directories")
+group.add_argument("-e", "--excel", action="store_true", help="Store results to excel")
+
 args = parser.parse_args()
 
 output_folder = os.path.join(args.directory_path, "output")
 os.makedirs(output_folder, exist_ok=True)
 
-success_responses = []
-failure_responses = []
-
-dir_items = os.listdir(args.directory_path)
-for idx, file_name in enumerate(dir_items):
-    if file_name == "output":
-        continue
-
-    print(
-        f"Testing file {idx}/{len(dir_items)} - {file_name}. \nSuccess ({len(success_responses)}), "
-        f"Failed ({len(failure_responses)})"
-    )
-
-    file_path = os.path.join(args.directory_path, file_name)
-    if os.path.isfile(file_path):
-        response = post_file(file_path, args.endpoint_url)
-
-        if response.status_code == requests.codes.ok:
-            success_responses.append(file_name)
-            print("Success.")
-        else:
-            failure_responses.append(file_name)
-            save_response(file_path, response, output_folder)
-            print("Failed.")
-    print()
-
-with open(os.path.join(output_folder, "success_responses.txt"), "w") as success_file:
-    success_file.write("\n\n".join(success_responses))
-
-with open(os.path.join(output_folder, "failure_responses.txt"), "w") as failure_file:
-    failure_file.write("\n\n".join(failure_responses))
+if args.directories:
+    output_to_directories()
+elif args.excel:
+    output_to_excel()
