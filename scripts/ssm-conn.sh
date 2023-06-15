6340c3b728b671726ec7f93b1aa0f85c0404ed3c
Aaron Williams <aaron.williams@version1.com>
2023-06-15 10:36:11 +0100

    Updating bash script
    * Adding in exit conditions
    * Adding in error responses
    * Mix getopts with positional parameters
    
    +semver: minor

--- scripts/ssm-conn.sh
+++ scripts/ssm-conn.sh
@@ -2,49 +2,88 @@
 
 usage () {
     echo "Connects a container to a local port, for example to allow you to 'curl' requests from your local machine to the instance."
-    echo "Usage: $(basename ${0}) local_port_number [port_number] [cluster_name] [service_name] [task_name] [container_name]"
+    echo "Usage: $(basename ${0}) local_port_number -p [port_number] -c [cluster_name] -s [service_name] -t [task_name] -d [docker_container_name]"
     exit 1
 }
 
-if [[ "${#}" -lt 1 ]]; then
+while [ $OPTIND -le "$#" ]
+do
+    if getopts p:c:s:t:d: opt
+    then
+        case $opt in
+            p) port_number=$OPTARG
+            ;;
+            c) cluster_name=$OPTARG
+            ;;
+            s) service_name=$OPTARG
+            ;;
+            t) task_name=$OPTARG
+            ;;
+            d) docker_container_name=$OPTARG
+            ;;
+            \?) echo "Invalid option -$OPTARG" >&2
+            exit 1
+            ;;
+        esac
+    else
+        script_args+=("${!OPTIND}")
+        ((OPTIND++))
+    fi
+done
+
+if [[ "${#script_args[@]}" -ne 1 ]]; then
     echo "Invalid number of arguments - local port number must be supplied"
     usage
 fi
 
-if [ -z "${2}" ]; then
+
+if [ -z "${port_number}" ]; then
     port_number='8080'
-else
-    port_number=${2}
 fi
 
-if [ -z "${3}" ]; then
+if [ -z "${cluster_name}" ]; then
     cluster_name='post-award-test'
-else
-    cluster_name=${3}
 fi
 
-if [ -z "${4}" ]; then
+if [ -z "${service_name}" ]; then
     service_name='data-store'
-else
-    service_name=${4}
 fi
 
-if [ -z "${5}" ]; then
+if [ -z "${task_name}" ]; then
     task_name='post-award-test'
-else
-    task_name=${5}
 fi
 
-if [ -z "${6}" ]; then
-    container_name='data-store'
-else
-    container_name=${6}
+if [ -z "${docker_container_name}" ]; then
+    docker_container_name='data-store'
 fi
 
 export ECS_CLUSTER_ARN="$(aws ecs list-clusters --query "clusterArns[?contains(@, '${cluster_name}')]" --output text | sed 's|.*/||')"
+
+if [ -z "${ECS_CLUSTER_ARN}" ]; then
+    echo "Cluster not found"
+    exit 1
+fi
+
 export ECS_SERVICE_ARN="$(aws ecs list-services --cluster $ECS_CLUSTER_ARN --query "serviceArns[?contains(@, '${service_name}')]" --output text | sed 's|.*/||')"
+
+if [ -z "${ECS_SERVICE_ARN}" ]; then
+    echo "Service not found"
+    exit 1
+fi
+
 export ECS_TASK_ID="$(aws ecs list-tasks --cluster $ECS_CLUSTER_ARN --service $ECS_SERVICE_ARN --query "taskArns[?contains(@, '${task_name}')]" --output text | sed 's|.*/||')"
-export ECS_CONTAINER_ID="$(aws ecs describe-tasks --cluster $ECS_CLUSTER_ARN --task $ECS_TASK_ID --query "tasks[].containers[?name=='${container_name}'].runtimeId" --output text | sed 's|.*/||')"
+
+if [ -z "${ECS_TASK_ID}" ]; then
+    echo "Task not found"
+    exit 1
+fi
+
+export ECS_CONTAINER_ID="$(aws ecs describe-tasks --cluster $ECS_CLUSTER_ARN --task $ECS_TASK_ID --query "tasks[].containers[?name=='${docker_container_name}'].runtimeId" --output text | sed 's|.*/||')"
+
+if [ -z "${ECS_CONTAINER_ID}" ]; then
+    echo "Container not found"
+    exit 1
+fi
 
 aws ssm start-session \
     --target "ecs:${ECS_CLUSTER_ARN}_${ECS_TASK_ID}_${ECS_CONTAINER_ID}" \
