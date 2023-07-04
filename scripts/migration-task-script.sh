99ad6f0dba1b3f30d27a3b6feff2f07003f60bc2
Aaron Williams <aaron.williams@version1.com>
2023-07-04 12:07:02 +0100

    Updating script to output exit code correctly

--- scripts/migration-task-script.sh
+++ scripts/migration-task-script.sh
@@ -1,13 +1,21 @@
 filename=generated_command_taskrun.sh
 echo '' >$filename
-(copilot task run --generate-cmd post-award/test/data-store 2>&1 | sed '$ s/$/ \\/' | sed 's/--command.*/--command \"/bin/sh -c \'flask db current ; echo EXEC-EXIT-CODE=\\\$?\'\"\\/g') >>$filename
+
+(copilot task run --generate-cmd post-award/test/data-store 2>&1 | sed '$ s/$/ \\/') >>$filename
+
 cat <<EOF >>$filename
 --follow \\
 --task-group-name "db-migrate" \\
 EOF
+
+echo "--command \"/bin/sh -c 'flask db current ; echo EXEC-EXIT-CODE=\\\$?'\"" >>$filename
+
 source $filename | tee /tmp/exec-output
+
 exitcode=$(sed -nr 's/.* EXEC-EXIT-CODE=([0-9]+)/\1/p' /tmp/exec-output | tr -d '\r\n')
+
 echo got exit code "'$exitcode'"
+
 if [ -z "$exitcode" ]; then
   echo could not find exit code in copilot output
   exit 1
