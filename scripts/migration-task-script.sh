a5335b29a1c66b18fe570b5d13c34f8c9a67804d
Gideon Goldberg <1764158+gidsg@users.noreply.github.com>
2023-07-04 16:12:26 +0100

    use simple bash args

--- scripts/migration-task-script.sh
+++ scripts/migration-task-script.sh
@@ -1,48 +1,24 @@
 #!/usr/bin/env bash
 
-# Define the options
-options="-l environment:,command:,"
+ENVIRONMENT=${1:-test}
+COMMAND=${2:-'flask db current'}
 
-# Initialize the variables
-environment="test"
-command="flask db current"
-
-# Get the options
-while getopts $options OPT; do
-  case $OPT in
-    e|environment)
-      environment="$OPTARG"
-      ;;
-    c|command)
-      command="$OPTARG"
-      ;;
-    l)
-      echo "Environment: $environment (long: --environment)"
-      echo "Command: $command (long: --command)"
-      exit 0
-      ;;
-    ?)
-      echo "Invalid option: $OPT"
-      exit 1
-      ;;
-  esac
-done
 
 # Print the arguments
-echo "Environment: $environment"
-echo "Command: $command"
+echo "Environment: $ENVIRONMENT"
+echo "Command: $COMMAND"
 
 filename=generated_command_taskrun.sh
 echo '' >$filename
 
-(copilot task run --generate-cmd post-award/$environment/data-store 2>&1 | sed '$ s/$/ \\/' | sed 's/--command.*/d \\/') >>$filename
+(copilot task run --generate-cmd post-award/$ENVIRONMENT/data-store 2>&1 | sed '$ s/$/ \\/' | sed 's/--command.*/d \\/') >>$filename
 
 cat <<EOF >>$filename
 --follow \\
 --task-group-name "db-migrate" \\
 EOF
 
-echo "--command \"/bin/sh -c '$command ; echo EXEC-EXIT-CODE=\\\$?'\"" >>$filename
+echo "--command \"/bin/sh -c '$COMMAND ; echo EXEC-EXIT-CODE=\\\$?'\"" >>$filename
 
 source $filename | tee /tmp/exec-output
 
