8cbbafd5c125bacb33bb0c74b444d52c37af4b22
Gideon Goldberg <1764158+gidsg@users.noreply.github.com>
2023-07-04 11:15:30 +0100

    add WIP task script

---
+++ scripts/migration-task-script.sh
@@ -0,0 +1,14 @@
+filename=generated_command_taskrun.sh
+echo '' >$filename
+(copilot task run --generate-cmd post-award/test/data-store 2>&1 | sed '$ s/$/ \\/' | sed 's/--command.*/--command \"/bin/sh -c \'flask db current ; echo EXEC-EXIT-CODE=\\\$?\'\"\\/g') >>$filename
+cat <<EOF >>$filename
+--follow \\
+--task-group-name "db-migrate" \\
+EOF
+source $filename | tee /tmp/exec-output
+exitcode=$(sed -nr 's/.* EXEC-EXIT-CODE=([0-9]+)/\1/p' /tmp/exec-output | tr -d '\r\n')
+echo got exit code "'$exitcode'"
+if [ -z "$exitcode" ]; then
+  echo could not find exit code in copilot output
+  exit 1
+fi
