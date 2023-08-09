bfdccebc4a098caa900189c6e6ce914d8181fdeb
Joel Pearce <joelpearce1994@gmail.com>
2023-08-09 16:07:32 +0100

    [SMD-32]: enable authentication
    ..
    
    ..
    
    ..
    
    ..
    
    added sign-out link
    
    ..

--- submit/tests/conftest.py
+++ submit/tests/conftest.py
@@ -1,18 +1,25 @@
 import pytest
-from flask.testing import FlaskClient
 
 import config
 from app import create_app
 
 
 @pytest.fixture()
-def flask_test_client() -> FlaskClient:
+def mocked_auth(monkeypatch):
+    def access_token(return_app):
+        return {"accountId": "test-user", "roles": []}
+
+    monkeypatch.setattr(
+        "fsd_utils.authentication.decorators._check_access_token",
+        access_token,
+    )
+
+
+@pytest.fixture()
+def flask_test_client(mocked_auth):
     """
     Creates the test client we will be using to test the responses
     from our app, this is a test fixture.
-
-    NOTE: Auth is mocked here because it's required for all routes.
-
     :return: A flask test client.
     """
     with create_app(config.Config).test_client() as test_client:
