64f2aca78caed4d0b2bc69511ee34bcd7d449d1d
Connor O'Shaughnessy <connor.oshaughnessy@version1.com>
2023-08-11 15:17:28 +0100

    [SMD-15] Basic submission end-to-end-flow
    [SMD-15] Basic submission end-to-end-flow
    
    improved flow and refactored upload
    
    Unit testing upload endpoint
    
    add file format validation and unit test
    
    unit test page html and request mocking
    
    added requests_mock to requirements_dev.txt
    
    cleaned up requirements_dev
    
    Clean up requirments_dev
    
    clean up requirements_dev
    
    [SMD-15] Basic submission end-to-end-flow
    
    [SMD-15] Basic submission end-to-end-flow
    
    improved flow and refactored upload
    
    Unit testing upload endpoint
    
    add file format validation and unit test
    
    unit test page html and request mocking
    
    added requests_mock to requirements_dev.txt
    
    cleaned up requirements_dev
    
    update .gitignore
    
    ...
    
    ...
    
    ...
    
    ...
    
    ...
    
    ...
    
    ...

--- submit/tests/conftest.py
+++ submit/tests/conftest.py
@@ -1,7 +1,9 @@
 import pytest
+from werkzeug.datastructures import FileStorage
 
 import config
 from app import create_app
+from config.envs.unit_test import UnitTestConfig
 
 
 @pytest.fixture()
@@ -20,7 +22,24 @@ def flask_test_client(mocked_auth):
     """
     Creates the test client we will be using to test the responses
     from our app, this is a test fixture.
+
+    NOTE: Auth is mocked here because it's required for all routes.
+
     :return: A flask test client.
     """
     with create_app(config.Config).test_client() as test_client:
         yield test_client
+
+
+@pytest.fixture(scope="function")
+def example_pre_ingest_data_file() -> FileStorage:
+    """An example pre ingest spreadsheet in towns-fund format."""
+    with open(UnitTestConfig.EXAMPLE_INGEST_DATA_PATH, "rb") as file:
+        yield file
+
+
+@pytest.fixture(scope="function")
+def example_ingest_wrong_format() -> FileStorage:
+    """An example pre ingest spreadsheet in towns-fund format."""
+    with open(UnitTestConfig.EXAMPLE_INGEST_WRONG_FORMAT, "rb") as file:
+        yield file
--- submit/tests/test_routes.py
+++ submit/tests/test_routes.py
@@ -1,3 +1,6 @@
+from bs4 import BeautifulSoup
+
+
 def test_index_page(flask_test_client):
     response = flask_test_client.get("/")
     assert response.status_code == 302
@@ -6,3 +9,44 @@ def test_index_page(flask_test_client):
 def test_upload_page(flask_test_client):
     response = flask_test_client.get("/upload")
     assert response.status_code == 200
+
+
+def test_upload_xlsx_successful(requests_mock, example_pre_ingest_data_file, flask_test_client):
+    requests_mock.post(
+        "http://data-store/ingest",
+        content=b'"Success: Spreadsheet data ingested"\n',
+    )
+    response = flask_test_client.post("/upload", data={"ingest_spreadsheet": example_pre_ingest_data_file})
+    page_html = BeautifulSoup(response.data)
+    assert response.status_code == 200
+    assert "Success: Spreadsheet data ingested" in str(page_html)
+
+
+def test_upload_xlsx_errors(requests_mock, example_pre_ingest_data_file, flask_test_client):
+    requests_mock.post(
+        "http://data-store/ingest",
+        content=b'{\n "validation_errors": [\n    "Wrong '
+        b'Type Failure: Sheet \\"Funding\\" column \\"Spend for Reporting '
+        b'Period\\" expected type \\"float64\\", got type \\"object\\"",\n'
+        b'"Enum Value Failure: Sheet \\"RiskRegister\\" Column '
+        b'\\"Pre-mitigatedImpact\\" Row 5 Value \\"Error\\" is not a valid '
+        b'enum value."]\n}\n',
+    )
+    response = flask_test_client.post("/upload", data={"ingest_spreadsheet": example_pre_ingest_data_file})
+    page_html = BeautifulSoup(response.data)
+    assert response.status_code == 200
+    assert (
+        'Wrong Type Failure: Sheet "Funding" column "Spend for Reporting '
+        'Period" expected type "float64", got type "object'
+    ) in str(page_html)
+    assert (
+        'Enum Value Failure: Sheet "RiskRegister" Column '
+        '"Pre-mitigatedImpact" Row 5 Value "Error" is not a valid enum value.'
+    ) in str(page_html)
+
+
+def test_upload_wrong_format(flask_test_client, example_ingest_wrong_format):
+    response = flask_test_client.post("/upload", data={"ingest_spreadsheet": example_ingest_wrong_format})
+    page_html = BeautifulSoup(response.data)
+    assert response.status_code == 200
+    assert ("Unexpected file format:") in str(page_html)
