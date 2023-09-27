2de2fcf1986f32e782b7e4851f3cf4ca53e278d2
Bobbi Brown <rholdingbrown@gmail.com>
2023-09-27 12:00:14 +0100

    tests for S3 localstack connectivity

--- config/envs/development.py
+++ config/envs/development.py
@@ -1,3 +1,4 @@
+import os
 from fsd_utils import configclass
 
 from config.envs.default import DefaultConfig
@@ -5,4 +6,7 @@ from config.envs.default import DefaultConfig
 
 @configclass
 class DevelopmentConfig(DefaultConfig):
-    pass
+    AWS_REGION = os.getenv("AWS_REGION")
+    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+    ENDPOINT_URL = "http://127.0.0.1:4566/"
