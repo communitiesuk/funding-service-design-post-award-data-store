20de0b85173be9d65c5fd0bc697155a4a10b5620
Cyrus <22678716+cyrusdobbs@users.noreply.github.com>
2023-10-10 11:14:33 +0100

    [BAU] refactor pre-check test mock data

---
+++ tests/validation_tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest  # noqa
+
+# isort: off
+from tests.validation_tests.resources.mock_data import (  # noqa
+    invalid_workbook_round_four,  # noqa
+    valid_submission_details,  # noqa
+    valid_workbook,  # noqa
+    valid_workbook_round_four,  # noqa
+)
+
+# isort: on
--- tests/validation_tests/test_pre_checks.py
+++ tests/validation_tests/test_pre_checks.py
@@ -1,27 +1,15 @@
-import pandas as pd
 import pytest
 
 import core.validation.failures as vf
 from core.const import TF_PLACE_NAMES_TO_ORGANISATIONS
 from core.exceptions import ValidationError
 from core.validation.failures import WrongInputFailure
-
-# isort: off
 from core.validation.initial_check import (
     extract_submission_details,
     pre_transformation_check,
-    validate_sign_off,
     validate_before_transformation,
+    validate_sign_off,
 )
-
-from tests.validation_tests.mock_data import (
-    invalid_workbook_round_four,
-    valid_submission_details,
-    valid_workbook,
-    valid_workbook_round_four,
-)
-
-# flake8: noqa
 
 
 def test_extract_round_three_submission_details(valid_workbook):
@@ -46,7 +34,7 @@ def test_extract_round_three_submission_details(valid_workbook):
     )
 
 
-def test_extract_round_four_submission_details(valid_workbook_round_four):
+def test_extract_round_four_submission_details(valid_workbook_round_four, mocker):
     details_dict = extract_submission_details(valid_workbook_round_four, 4, place_names=("Newark",))
 
     assert details_dict["Form Version"] == (
