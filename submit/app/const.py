385a64d339105607c909d3d7493f409cbc5af522
Gideon Goldberg <1764158+gidsg@users.noreply.github.com>
2023-10-31 13:17:15 +0000

    BAU: Add hambleton to safelist
    Some users are still under this domain

--- submit/app/const.py
+++ submit/app/const.py
@@ -140,6 +140,14 @@ EMAIL_DOMAIN_TO_LA_AND_PLACE_NAMES = {
             "Whitby",
         ),
     ),
+    "hambleton.gov.uk": (
+        ("North Yorkshire Council",),
+        (
+            "Northallerton",
+            "Scarborough",
+            "Whitby",
+        ),
+    ),
     "norwich.gov.uk": (("Norwich City Council",), ("Norwich",)),
     "nottinghamcity.gov.uk": (("Nottingham City Council",), ("Nottingham City Centre, West End Point",)),
     "nuneatonandbedworth.gov.uk": (
