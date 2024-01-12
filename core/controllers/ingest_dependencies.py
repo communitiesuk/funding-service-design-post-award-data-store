20ae3c48532ea4436a88c5f961ef1764db8f6937
Cyrus <22678716+cyrusdobbs@users.noreply.github.com>
2024-01-12 17:06:49 +0000

    [BAU] fix ingest validation imports

--- core/controllers/ingest_dependencies.py
+++ core/controllers/ingest_dependencies.py
@@ -3,6 +3,7 @@ from typing import Callable
 
 import pandas as pd
 
+import core.validation.specific_validation.towns_fund_round_four.validate as tf_r4_validate
 from core.extraction.towns_fund_round_four import ingest_round_four_data_towns_fund
 from core.extraction.towns_fund_round_one import ingest_round_one_data_towns_fund
 from core.extraction.towns_fund_round_three import ingest_round_three_data_towns_fund
@@ -10,13 +11,15 @@ from core.extraction.towns_fund_round_two import ingest_round_two_data_towns_fun
 from core.messaging import MessengerBase
 from core.messaging.tf_messaging import TFMessenger
 from core.validation import ValidationFailureBase
-from core.validation.pre_transformation_validation_schema import TF_ROUND_3, TF_ROUND_4
-from core.validation.specific_validations import towns_fund_round_four as tf_round_4
-from core.validation_schema import (
-    ROUND_FOUR_TF_SCHEMA,
-    ROUND_ONE_TF_SCHEMA,
-    ROUND_THREE_TF_SCHEMA,
-    ROUND_TWO_TF_SCHEMA,
+from core.validation.initial_validation.schemas import (
+    TF_ROUND_3_INIT_VAL_SCHEMA,
+    TF_ROUND_4_INIT_VAL_SCHEMA,
+)
+from core.validation.schema_validation.schemas import (
+    TF_ROUND_1_VAL_SCHEMA,
+    TF_ROUND_2_VAL_SCHEMA,
+    TF_ROUND_3_VAL_SCHEMA,
+    TF_ROUND_4_VAL_SCHEMA,
 )
 
 
@@ -41,7 +44,7 @@ class IngestDependencies:
     fund_specific_validation: Callable[
         [dict[str, pd.DataFrame], dict[str, pd.DataFrame]], list[ValidationFailureBase]
     ] | None = None
-    pre_transformation_validation_schema: dict | None = None
+    initial_validation_schema: dict | None = None
     messenger: MessengerBase | None = None
 
 
@@ -56,24 +59,24 @@ def ingest_dependencies_factory(fund: str, reporting_round: int) -> IngestDepend
     match (fund, reporting_round):
         case ("Towns Fund", 1):
             return IngestDependencies(
-                transform_data=ingest_round_one_data_towns_fund, validation_schema=ROUND_ONE_TF_SCHEMA
+                transform_data=ingest_round_one_data_towns_fund, validation_schema=TF_ROUND_1_VAL_SCHEMA
             )
         case ("Towns Fund", 2):
             return IngestDependencies(
-                transform_data=ingest_round_two_data_towns_fund, validation_schema=ROUND_TWO_TF_SCHEMA
+                transform_data=ingest_round_two_data_towns_fund, validation_schema=TF_ROUND_2_VAL_SCHEMA
             )
         case ("Towns Fund", 3):
             return IngestDependencies(
                 transform_data=ingest_round_three_data_towns_fund,
-                validation_schema=ROUND_THREE_TF_SCHEMA,
-                pre_transformation_validation_schema=TF_ROUND_3,
+                validation_schema=TF_ROUND_3_VAL_SCHEMA,
+                initial_validation_schema=TF_ROUND_3_INIT_VAL_SCHEMA,
             )
         case ("Towns Fund", 4):
             return IngestDependencies(
                 transform_data=ingest_round_four_data_towns_fund,
-                validation_schema=ROUND_FOUR_TF_SCHEMA,
-                fund_specific_validation=tf_round_4.validate,
-                pre_transformation_validation_schema=TF_ROUND_4,
+                validation_schema=TF_ROUND_4_VAL_SCHEMA,
+                fund_specific_validation=tf_r4_validate.validate,
+                initial_validation_schema=TF_ROUND_4_INIT_VAL_SCHEMA,
                 messenger=TFMessenger(),
             )
         case _:
