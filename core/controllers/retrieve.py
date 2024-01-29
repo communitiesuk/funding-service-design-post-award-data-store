5ca7a15ce092e36f718e3995116d5e9d5fe2e080
MarcUsher <99440441+MarcUsher@users.noreply.github.com>
2024-01-29 11:12:26 +0000

    Update retrieve to use updated object name format

--- core/controllers/retrieve.py
+++ core/controllers/retrieve.py
@@ -4,6 +4,7 @@ from flask import abort
 from config import Config
 from core.aws import get_file
 from core.const import EXCEL_MIMETYPE
+from core.db.entities import Programme, Project, Submission
 
 
 def retrieve(submission_id):
@@ -14,7 +15,16 @@ def retrieve(submission_id):
 
     :return: Flask response object containing the requested spreadsheet.
     """
-    if file_meta := get_file(Config.AWS_S3_BUCKET_SUCCESSFUL_FILES, submission_id):
+    submission_meta = (
+        Programme.query.join(Project)
+        .join(Submission)
+        .filter(Submission.submission_id == submission_id)
+        .with_entities(Submission.id, Programme.fund_type_id)
+        .distinct()
+    ).all()[0]
+    object_name = f"{submission_meta.fund_type_id}/{str(submission_meta.id)}"
+
+    if file_meta := get_file(Config.AWS_S3_BUCKET_SUCCESSFUL_FILES, object_name):
         file, meta_data = file_meta
     else:
         return abort(404, "Could not find a file that matches this submission_id")
