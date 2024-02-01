b2a0b490c9093a2cca20083940b422a164b7493e
William May <wjrm500@gmail.com>
2024-02-01 10:14:14 +0000

    Formatting TF Round 3 initial validation schema as list of checks in line with Round 4

--- core/validation/initial_validation/schemas.py
+++ core/validation/initial_validation/schemas.py
@@ -7,7 +7,6 @@ from core.const import PLACE_TO_FUND_TYPE, TF_PLACE_NAMES_TO_ORGANISATIONS
 from core.validation.checks import AuthorisationCheck, Check, MappedCheck
 
 
-
 TF_ROUND_4_INIT_VAL_SCHEMA = [
     Check(
         sheet="1 - Start Here",
@@ -68,28 +67,27 @@ TF_ROUND_4_INIT_VAL_SCHEMA = [
     ),
 ]
 
-TF_ROUND_3_INIT_VAL_SCHEMA = {
-    "basic_checks": [
-        Check(
-            sheet="1 - Start Here",
-            column=6,
-            row=1,
-            expected_values=("Town Deals and Future High Streets Fund Reporting Template (v3.0)",),
-            error_message="Fund Name in the tab \"1 - Start Here\" must be \"Town Deals and Future High Streets Fund Reporting Template (v3.0)\".",
-        ),
-        Check(
-            sheet="1 - Start Here",
-            column=4,
-            row=1,
-            expected_values=("1 October 2022 to 31 March 2023",),
-            error_message="Reporting Period in the tab \"1 - Start Here\" must be \"1 April 2023 to 30 September 2023\".",
-        ),
-        Check(
-            sheet="2 - Project Admin",
-            column=5,
-            row=4,
-            expected_values=("Town_Deal", "Future_High_Street_Fund"),
-            error_message="Fund Type in the tab \"2 - Project Admin\" must be either \"Town_Deal\" or \"Future_High_Street_Fund\".",
-        ),
-    ],
-}
+
+TF_ROUND_3_INIT_VAL_SCHEMA = [
+    Check(
+        sheet="1 - Start Here",
+        column=6,
+        row=1,
+        expected_values=("Town Deals and Future High Streets Fund Reporting Template (v3.0)",),
+        error_message="Fund Name in the tab \"1 - Start Here\" must be \"Town Deals and Future High Streets Fund Reporting Template (v3.0)\".",
+    ),
+    Check(
+        sheet="1 - Start Here",
+        column=4,
+        row=1,
+        expected_values=("1 October 2022 to 31 March 2023",),
+        error_message="Reporting Period in the tab \"1 - Start Here\" must be \"1 April 2023 to 30 September 2023\".",
+    ),
+    Check(
+        sheet="2 - Project Admin",
+        column=5,
+        row=4,
+        expected_values=("Town_Deal", "Future_High_Street_Fund"),
+        error_message="Fund Type in the tab \"2 - Project Admin\" must be either \"Town_Deal\" or \"Future_High_Street_Fund\".",
+    ),
+]
