2a55ba5a811d3e482d94caa3d7d557360cd68584
Gideon Goldberg <1764158+gidsg@users.noreply.github.com>
2024-02-02 15:17:40 +0000

    bump migration task memory

--- scripts/migration-task-script.py
+++ scripts/migration-task-script.py
@@ -22,7 +22,9 @@ except subprocess.CalledProcessError as e:
 # Remove final line break and append arguments
 try:
     subprocess.run(
-        args=command_with_image_removed[:-1] + f" \\\n--follow \\\n--command '{command_to_run}'", shell=True, check=True
+        args=command_with_image_removed[:-1] + f" \\\n--follow \\\n--memory 2048 \\\n--command '{command_to_run}'",
+        shell=True,
+        check=True,
     )
 except subprocess.CalledProcessError as e:
     # Don't want to leak the command output here so just exit with the command's return code
