d718b13743d001380abf0f880115b4b0ba397a3b
Bobbi Brown <rholdingbrown@gmail.com>
2024-02-02 08:32:59 +0000

    allow auth to be empty

--- core/validation/checks.py
+++ core/validation/checks.py
@@ -2,22 +2,16 @@ from abc import ABC, abstractmethod
 
 import pandas as pd
 
+
 class Check:
-    def __init__(
-        self,
-        sheet: str,
-        column: int,
-        row: int,
-        expected_values: tuple | None,
-        error_message: str
-    ):
+    def __init__(self, sheet: str, column: int, row: int, expected_values: tuple | None, error_message: str):
         self.sheet = sheet
         self.column = column
         self.row = row
         self.expected_values = expected_values
         self.error_message = error_message
         self.actual_value = None
-    
+
     def run(self, workbook: dict[str, pd.DataFrame]) -> bool:
         self.actual_value = str(workbook[self.sheet].iloc[self.column][self.row]).strip()
         if self.actual_value not in self.expected_values:
@@ -29,17 +23,11 @@ class DynamicCheck(Check, ABC):
     """
     A check that dynamically generates the expected values based on the workbook.
     """
+
     _dynamic_params: dict  # Values used to calculate the expected values
     _error_message_with_placeholders: str  # The error message before substitution
 
-    def __init__(
-        self,
-        sheet: str,
-        column: int,
-        row: int,
-        dynamic_params: dict,
-        error_message_with_placeholders: str
-    ):
+    def __init__(self, sheet: str, column: int, row: int, dynamic_params: dict, error_message_with_placeholders: str):
         super().__init__(sheet, column, row, None, None)
         self._dynamic_params = dynamic_params
         self._error_message_with_placeholders = error_message_with_placeholders
@@ -76,7 +64,7 @@ class AuthorisationCheck(DynamicCheck):
 
     def calculate_expected_values(self, workbook: dict[str, pd.DataFrame]) -> tuple:
         auth_type = self._dynamic_params["auth_type"]
-        expected_values = self.auth[auth_type]
+        expected_values = self.auth[auth_type] if self.auth else ""
         self.expected_values = expected_values
 
     def substitute_error_message(self) -> None:
