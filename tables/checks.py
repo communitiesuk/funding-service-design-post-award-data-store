704ce0c171525f41a95f6f6402f32c8687c20edd
Cyrus <22678716+cyrusdobbs@users.noreply.github.com>
2024-02-06 15:17:23 +0000

    [SMD-522] add word_count_less_than as a custom Check

---
+++ tables/checks.py
@@ -0,0 +1,45 @@
+"""Module for custom Check methods.
+
+From pandera docs https://pandera.readthedocs.io/en/stable/extensions.html#extensions
+
+One of the strengths of pandera is its flexibility in enabling you to define in-line custom checks on the fly:
+
+>>> import pandera as pa
+>>> # checks elements in a column/dataframe
+>>> element_wise_check = pa.Check(lambda x: x < 0, element_wise=True)
+>>> # applies the check function to a dataframe/series
+>>> vectorized_check = pa.Check(lambda series_or_df: series_or_df < 0)
+
+However, there are two main disadvantages of schemas with inline custom checks:
+    1. they are not serializable with the IO interface.
+    2. you can’t use them to synthesize data because the checks are not associated with a hypothesis strategy.
+"""
+import pandera as pa
+import pandera.strategies as st
+from hypothesis import strategies
+from pandera import extensions
+
+
+def word_count_less_than_strategy(pandera_dtype: pa.DataType, strategy: st.SearchStrategy | None = None, *, max_words):
+    """A strategy for synthesising data that abides by the "word_count_less_than" Check constraint."""
+    if strategy is None:
+        return st.pandas_dtype_strategy(
+            pandera_dtype,
+            strategy=strategies.text().filter(lambda x: len(x.split()) < max_words),
+        )
+    return strategy.filter(lambda x: len(x.split()) < max_words)
+
+
+@extensions.register_check_method(
+    statistics=["max_words"], check_type="element_wise", strategy=word_count_less_than_strategy
+)
+def word_count_less_than(element, *, max_words):
+    """Checks that a string split up by whitespace characters is less than or equal to "max_words" elements long.
+
+    :param element: an element to check
+    :param max_words: the maximum allowed length of the string split up by whitespace
+    :return: True if passes the check, else False
+    """
+    if not isinstance(element, str):
+        raise TypeError("Value must be a string")
+    return len(element.split()) <= max_words
