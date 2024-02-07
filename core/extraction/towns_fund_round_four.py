ca634cb69917f43bae2a283805fac88c63ee6b8e
William May <wjrm500@gmail.com>
2024-02-07 13:52:36 +0000

    Implementing extract method

--- core/extraction/towns_fund_round_four.py
+++ core/extraction/towns_fund_round_four.py
@@ -9,6 +9,7 @@ The differences are:
 - Q6 in Programme Progress is removed
 - Two new columns in Project Progress
 """
+import numpy as np
 import pandas as pd
 
 import core.extraction.towns_fund_round_three as r3
@@ -98,7 +99,26 @@ def extract_programme_progress(df_data: pd.DataFrame, programme_id: str) -> pd.D
 
 
 def extract_programme_management(df_data: pd.DataFrame, programme_id: str) -> pd.DataFrame:
-    if programme_id.split("-")[0] != "TD":
-        # Return an empty DataFrame if the programme ID is not Town Deal
-        return pd.DataFrame(columns=[])
-    return pd.DataFrame(columns=[])
+    # if programme_id.split("-")[0] != "TD":
+    #     # Return an empty DataFrame if the programme ID is not Town Deal
+    #     return pd.DataFrame(columns=[])
+    header_prefix = ["Payment type"]
+    header_row_1 = [x := y if y is not np.nan else x for y in df_data.iloc[21, 5:25]]  # noqa: F821, F841
+    header_row_2 = [field if field is not np.nan else "" for field in list(df_data.iloc[22, 5:25])]
+    header_row_3 = [field if field is not np.nan else "" for field in list(df_data.iloc[23, 5:25])]
+    header_row_combined = [
+        "__".join([x, y, z]).rstrip("_") for x, y, z in zip(header_row_1, header_row_2, header_row_3)
+    ]
+    header = header_prefix + header_row_combined
+    transformed_df = df_data.iloc[24:26, [2] + list(range(5, 25))]
+    transformed_df.columns = header
+    columns_to_drop = [col for col in transformed_df.columns if col.endswith("__Total")]
+    transformed_df.drop(columns=columns_to_drop, inplace=True)
+    transformed_df = pd.melt(
+        transformed_df,
+        id_vars=header_prefix,
+        var_name="Reporting Period",
+        value_name="Spend for Reporting Period",
+        ignore_index=False,
+    )
+    return transformed_df
