79ba42737545b1698c2e7ca032f8d210d13b2908
William May <wjrm500@gmail.com>
2024-02-07 17:30:17 +0000

    Additional test for empty DataFrame given non-TD input

--- tests/extraction_tests/test_round_four_extraction.py
+++ tests/extraction_tests/test_round_four_extraction.py
@@ -86,9 +86,24 @@ def test_extract_programme_progress(mock_progress_sheet, mock_programme_lookup):
 
 
 def test_extract_programme_management(mock_funding_sheet, mock_programme_lookup):
-    """Test programme management rows extracted as expected."""
+    """Test programme management rows extracted as expected when programme ID is Town Deal."""
     extracted_programme_management = tf.extract_programme_management(mock_funding_sheet, mock_programme_lookup)
-    expected_programme_management = pd.read_csv(resources_assertions / "programme_management_expected.csv", index_col=0)
+    expected_programme_management = pd.read_csv(
+        resources_assertions / "programme_management_td_expected.csv", index_col=0
+    )
+    expected_programme_management["Spend for Reporting Period"] = expected_programme_management[
+        "Spend for Reporting Period"
+    ].astype(object)
+    assert_frame_equal(extracted_programme_management, expected_programme_management)
+
+
+def test_extract_programme_management_non_td(mock_funding_sheet):
+    """Test programme management rows extracted as expected when programme ID is not Town Deal."""
+    programme_lookup = "HS-FAK-01"
+    extracted_programme_management = tf.extract_programme_management(mock_funding_sheet, programme_lookup)
+    expected_programme_management = pd.read_csv(
+        resources_assertions / "programme_management_non_td_expected.csv", index_col=0
+    )
     expected_programme_management["Spend for Reporting Period"] = expected_programme_management[
         "Spend for Reporting Period"
     ].astype(object)
