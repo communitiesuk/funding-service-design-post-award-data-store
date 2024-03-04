d0ea606b4387700ebd91db14f33739be7bbe6160
William May <wjrm500@gmail.com>
2024-03-04 11:40:23 +0000

    Fixing up reference to file under test in test_pathfinders module

--- tests/transformation_tests/pathfinders/test_pathfinders.py
+++ tests/transformation_tests/pathfinders/test_pathfinders.py
@@ -3,7 +3,7 @@ import datetime
 import pandas as pd
 from pandas._testing import assert_frame_equal
 
-import core.transformation.pathfinders as pf
+import core.transformation.pathfinders.transform as pf
 
 
 def test_pathfinders_transform(
