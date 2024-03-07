b6f183b255aeff292e3a5b238b96af9ba9776569
MarcUsher <99440441+MarcUsher@users.noreply.github.com>
2024-03-07 14:22:41 +0000

    FMD-241: Add GeospatialDim Table to DB
    - Migration creates empty geospatial_dim table and populates initial
    seed data of postcode_prefix and ITL1 region mappings
    - Add tests to check integrity of seed data in geospatial_dim table
    - Add new `reference_data` module for methods relating to seeding static
    reference data
    - Add method for re-seeding geospatial reference data, checking against
    `postcode_prefix` when comparing against existing data
    - Update local seed methods to persist geospatial_dim data created by
    alembic migration and command to re-seed geospatial data in isolation

--- tests/db_tests/test_entities.py
+++ tests/db_tests/test_entities.py
@@ -76,3 +76,54 @@ def test_project_finance_change_table(seeded_test_client_rollback):
     db.session.add(project_finance_change)
     with pytest.raises(IntegrityError):
         db.session.flush()
+
+
+def test_geospatial_dim_table(seeded_test_client_rollback):
+    """
+    Tests GeospatialDim table is correctly populated with reference data from
+    resources/geospatial_dim csv seed data, including checking the number
+    of rows in the table and that all itl1 region code and names are valid
+    pairs.
+
+    This test should fail if any new data is added to the csv or the
+    existing data is mutated. If the test fails it will need to be updated
+    and the database will likely need to be reseeded with the updated reference data
+    if this comes from an extension of the reference data.
+    """
+
+    all_geospatial_data = ents.GeospatialDim.query.order_by(ents.GeospatialDim.postcode_prefix).all()
+
+    assert len(all_geospatial_data) == 124
+
+    assert all_geospatial_data[0].postcode_prefix == "AB"
+    assert all_geospatial_data[0].itl1_region_code == "TLM"
+    assert all_geospatial_data[0].data_blob == {"itl1_region_name": "Scotland"}
+
+    assert all_geospatial_data[-1].postcode_prefix == "ZE"
+    assert all_geospatial_data[-1].itl1_region_code == "TLM"
+    assert all_geospatial_data[-1].data_blob == {"itl1_region_name": "Scotland"}
+
+    itl1_region_pairs = {
+        "TLC": "North East",
+        "TLD": "North West",
+        "TLE": "Yorkshire",
+        "TLF": "East Midlands",
+        "TLG": "West Midlands",
+        "TLH": "East",
+        "TLI": "London",
+        "TLJ": "South East",
+        "TLK": "South West",
+        "TLL": "Wales",
+        "TLM": "Scotland",
+        "TLN": "Northern Ireland",
+        "TLZ": "Non-ITL1 Region",
+    }
+
+    geospatial_data_itl1_region_pairs = ents.GeospatialDim.query.with_entities(
+        ents.GeospatialDim.itl1_region_code, ents.GeospatialDim.data_blob
+    ).distinct()
+
+    for row in geospatial_data_itl1_region_pairs:
+        region_code = row.itl1_region_code
+        region_name = row.data_blob["itl1_region_name"]
+        assert itl1_region_pairs[region_code] == region_name
