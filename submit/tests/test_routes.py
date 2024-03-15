2a602c4a6d654d411ec3ad5bb719cf84233f291e
Bobbi Brown <rholdingbrown@gmail.com>
2024-03-15 10:04:37 +0000

    SMD-697 add id tags for testing

--- submit/tests/test_routes.py
+++ submit/tests/test_routes.py
@@ -21,11 +21,12 @@ def test_select_fund_page_with_tf_role(flask_test_client, mocker):
     response = flask_test_client.get("/dashboard")
     page_html = BeautifulSoup(response.data)
     assert response.status_code == 200
-    assert '<a class="govuk-heading-m govuk-link--no-visited-state" href="/upload/TF/5"> Towns Fund</a>' in str(
+    assert '<a class="govuk-heading-m govuk-link--no-visited-state" href="/upload/TF/5" id="TF"> Towns Fund</a>' in str(
         page_html
     )
-    assert '<a class="govuk-heading-m govuk-link--no-visited-state" href="/upload/PF/1"> Pathfinders</a>' not in str(
-        page_html
+    assert (
+        '<a class="govuk-heading-m govuk-link--no-visited-state" href="/upload/PF/1" id="PF"> Pathfinders</a>'
+        not in str(page_html)
     )
 
 
@@ -34,10 +35,12 @@ def test_select_fund_page_with_pf_role(flask_test_client, mocked_pf_auth):
     assert response.status_code == 200
     page_html = BeautifulSoup(response.data)
     assert (
-        '<a class="govuk-heading-m govuk-link--no-visited-state" href="/upload/TF/5"> Towns Fund</a>' not in page_html
+        '<a class="govuk-heading-m govuk-link--no-visited-state" href="/upload/TF/5" id="TF"> Towns Fund</a>'
+        not in page_html
     )
-    assert '<a class="govuk-heading-m govuk-link--no-visited-state" href="/upload/PF/1"> Pathfinders</a>' in str(
-        page_html
+    assert (
+        '<a class="govuk-heading-m govuk-link--no-visited-state" href="/upload/PF/1" id="PF"> Pathfinders</a>'
+        in str(page_html)
     )
 
 
@@ -46,11 +49,12 @@ def test_select_fund_page_with_tf_and_pf_roles(flask_test_client, mocked_pf_and_
     response = flask_test_client.get("/dashboard")
     page_html = BeautifulSoup(response.data)
     assert response.status_code == 200
-    assert '<a class="govuk-heading-m govuk-link--no-visited-state" href="/upload/TF/5"> Towns Fund</a>' in str(
+    assert '<a class="govuk-heading-m govuk-link--no-visited-state" href="/upload/TF/5" id="TF"> Towns Fund</a>' in str(
         page_html
     )
-    assert '<a class="govuk-heading-m govuk-link--no-visited-state" href="/upload/PF/1"> Pathfinders</a>' in str(
-        page_html
+    assert (
+        '<a class="govuk-heading-m govuk-link--no-visited-state" href="/upload/PF/1" id="PF"> Pathfinders</a>'
+        in str(page_html)
     )
 
 
