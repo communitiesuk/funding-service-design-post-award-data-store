e0dfba2dbb827d429f8815485b2e5033b4579c08
William May <wjrm500@gmail.com>
2024-03-21 12:06:36 +0000

    Removing rogue positive number checks from 'Outputs' and 'Bespoke Outputs' validation

--- core/table_configs/pathfinders/round_1.py
+++ core/table_configs/pathfinders/round_1.py
@@ -287,10 +287,7 @@ PF_TABLE_CONFIG = {
                 ),
                 "Financial year 2025 to 2026, (Jul to Sep), Forecast": pa.Column(
                     float,
-                    checks=[
-                        pa.Check.is_float(error=PFErrors.IS_FLOAT),
-                        pa.Check.greater_than_or_equal_to(0, error=PFErrors.POSITIVE),
-                    ],
+                    checks=[pa.Check.is_float(error=PFErrors.IS_FLOAT)],
                 ),
                 "Financial year 2025 to 2026, (Oct to Dec), Forecast": pa.Column(
                     float,
@@ -352,10 +349,7 @@ PF_TABLE_CONFIG = {
                 ),
                 "Financial year 2025 to 2026, (Jul to Sep), Forecast": pa.Column(
                     float,
-                    checks=[
-                        pa.Check.is_float(error=PFErrors.IS_FLOAT),
-                        pa.Check.greater_than_or_equal_to(0, error=PFErrors.POSITIVE),
-                    ],
+                    checks=[pa.Check.is_float(error=PFErrors.IS_FLOAT)],
                 ),
                 "Financial year 2025 to 2026, (Oct to Dec), Forecast": pa.Column(
                     float,
