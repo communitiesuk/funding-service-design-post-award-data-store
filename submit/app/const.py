cfb112b544753770db95dc2252b94f75a87316d8
William May <48405095+wjrm500@users.noreply.github.com>
2024-03-27 15:52:01 +0000

    BAU - Add PF to FUND_TYPE_ID_TO_FRIENDLY_NAME mapping (#150)

--- submit/app/const.py
+++ submit/app/const.py
@@ -14,6 +14,7 @@ class MIMETYPE(StrEnum):
 FUND_TYPE_ID_TO_FRIENDLY_NAME = {
     "TD": "Town Deal",
     "HS": "Future High Streets Fund",
+    "PF": "Pathfinders",
 }
 
 # domain/email: (LAs, Places)
