c5cf4db3aa8b1fdea8b1e86ae277c10f040ff5ef
William May <48405095+wjrm500@users.noreply.github.com>
2024-03-28 13:12:22 +0000

    SMD-702 - Validating reporting period change against 'Actual, forecast or cancelled' field (#510)

--- tests/validation_tests/specific_validation_tests/pathfinders/test_round_1.py
+++ tests/validation_tests/specific_validation_tests/pathfinders/test_round_1.py
@@ -6,6 +6,7 @@ import pytest
 from core.exceptions import ValidationError
 from core.messaging import Message
 from core.validation.specific_validation.pathfinders.round_1 import (
+    _check_actual_forecast_reporting_period,
     _check_bespoke_outputs_outcomes,
     _check_credible_plan_fields,
     _check_projects,
@@ -145,11 +146,7 @@ def test_check_bespoke_outputs_outcomes_passes(mock_df_dict, mock_control_mappin
 def test_check_bespoke_outputs_outcomes_fails(mock_df_dict, mock_control_mappings):
     original_output = mock_df_dict["Bespoke outputs"]["Output"][0]
     mock_df_dict["Bespoke outputs"]["Output"][0] = "Invalid Bespoke Output"
-    with patch(
-        "core.validation.specific_validation.pathfinders.round_1.create_control_mappings"
-    ) as mock_create_control_mappings:
-        mock_create_control_mappings.return_value = mock_control_mappings
-        error_messages = _check_bespoke_outputs_outcomes(mock_df_dict, mock_control_mappings)
+    error_messages = _check_bespoke_outputs_outcomes(mock_df_dict, mock_control_mappings)
     mock_df_dict["Bespoke outputs"]["Output"][0] = original_output
     assert error_messages == [
         Message(
@@ -170,11 +167,7 @@ def test_check_credible_plan_fields_passes(mock_df_dict):
 def test_check_credible_plan_fields_fails(mock_df_dict):
     original_underspend = mock_df_dict["Total underspend"]["Total underspend"][0]
     mock_df_dict["Total underspend"]["Total underspend"][0] = pd.NA
-    with patch(
-        "core.validation.specific_validation.pathfinders.round_1.create_control_mappings"
-    ) as mock_create_control_mappings:
-        mock_create_control_mappings.return_value = mock_control_mappings
-        error_messages = _check_credible_plan_fields(mock_df_dict)
+    error_messages = _check_credible_plan_fields(mock_df_dict)
     mock_df_dict["Total underspend"]["Total underspend"][0] = original_underspend
     assert error_messages == [
         Message(
@@ -182,6 +175,44 @@ def test_check_credible_plan_fields_fails(mock_df_dict):
             section="Total underspend",
             cell_index=None,
             description="If you have selected 'Yes' for 'Credible Plan', you must answer Q2, Q3 and Q4.",
+            error_type=None,
+        )
+    ]
+
+
+def test_check_actual_forecast_reporting_period(mock_df_dict):
+    # Test case where there are no errors
+    error_messages = _check_actual_forecast_reporting_period(mock_df_dict)
+    assert error_messages == []
+
+    # Test case where there is an error for an "Actual" change in a future reporting period
+    original_reporting_period = mock_df_dict["Project finance changes"]["Reporting period change takes place"][0]
+    mock_df_dict["Project finance changes"]["Reporting period change takes place"][
+        0
+    ] = "Q1 2024/25: Apr 2024 - Jun 2024"
+    error_messages = _check_actual_forecast_reporting_period(mock_df_dict)
+    mock_df_dict["Project finance changes"]["Reporting period change takes place"][0] = original_reporting_period
+    assert error_messages == [
+        Message(
+            sheet="Finances",
+            section="Project finance changes",
+            cell_index=None,
+            description="Reporting period must not be in future if 'Actual, forecast or cancelled' is 'Actual'.",
+            error_type=None,
+        )
+    ]
+
+    # Test case where there is an error for a "Forecast" change in a past reporting period
+    original_actual_forecast = mock_df_dict["Project finance changes"]["Actual, forecast or cancelled"][0]
+    mock_df_dict["Project finance changes"]["Actual, forecast or cancelled"][0] = "Forecast"
+    error_messages = _check_actual_forecast_reporting_period(mock_df_dict)
+    mock_df_dict["Project finance changes"]["Actual, forecast or cancelled"][0] = original_actual_forecast
+    assert error_messages == [
+        Message(
+            sheet="Finances",
+            section="Project finance changes",
+            cell_index=None,
+            description="Reporting period must be in future if 'Actual, forecast or cancelled' is 'Forecast'.",
             error_type=None,
         )
     ]
