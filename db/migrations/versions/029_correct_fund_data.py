559c12f94991401770dadf573c5c2e2d2fd4d6bb
Joel Pearce <joelpearce1994@gmail.com>
2024-05-03 15:38:42 +0100

    [FMD-278]: fix old fund data with migration, update serialisation
    ..

---
+++ db/migrations/versions/029_correct_fund_data.py
@@ -0,0 +1,102 @@
+"""empty message
+
+Revision ID: 029_correct_fund_data
+Revises: 028_normalise_fund_ref_data
+Create Date: 2024-04-23 09:41:50.279834
+
+"""
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "029_correct_fund_data"
+down_revision = "028_normalise_fund_ref_data"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # Update rows where funding_source_type is not 'Towns Fund'
+    op.execute("""
+        UPDATE funding
+        SET data_blob = jsonb_set(
+            data_blob,
+            '{funding_source}',
+            to_jsonb(data_blob ->> 'funding_source_name'),
+            true
+        ),
+        data_blob = jsonb_set(
+            data_blob,
+            '{spend_type}',
+            to_jsonb(data_blob ->> 'funding_source_type'),
+            true
+        )
+        WHERE data_blob ->> 'funding_source_type' != 'Towns Fund';
+    """)
+
+    # Update rows where funding_source_type is 'Towns Fund'
+    op.execute("""
+        UPDATE funding
+        SET data_blob = jsonb_set(
+            data_blob,
+            '{funding_source}',
+            to_jsonb(data_blob ->> 'funding_source_type'),
+            true
+        ),
+        data_blob = jsonb_set(
+            data_blob,
+            '{spend_type}',
+            to_jsonb(data_blob ->> 'funding_source_name'),
+            true
+        )
+        WHERE data_blob ->> 'funding_source_type' = 'Towns Fund';
+    """)
+
+    op.execute("""
+        UPDATE funding
+        SET data_blob = data_blob - '{funding_source_name, funding_source_type}';
+    """)
+
+
+def downgrade():
+    # Update rows where funding_source is not 'Towns Fund'
+    op.execute("""
+        UPDATE funding
+        SET data_blob = jsonb_set(
+            data_blob,
+            '{funding_source_name}',
+            to_jsonb(data_blob ->> 'funding_source'),
+            true
+        ),
+        data_blob = jsonb_set(
+            data_blob,
+            '{funding_source_type}',
+            to_jsonb(data_blob ->> 'spend_type'),
+            true
+        )
+        WHERE data_blob ->> 'funding_source' != 'Towns Fund';
+    """)
+
+    # Update rows where funding_source is 'Towns Fund'
+    op.execute("""
+        UPDATE funding
+        SET data_blob = jsonb_set(
+            data_blob,
+            '{funding_source_type}',
+            to_jsonb(data_blob ->> 'funding_source'),
+            true
+        ),
+        data_blob = jsonb_set(
+            data_blob,
+            '{funding_source_name}',
+            to_jsonb(data_blob ->> 'spend_type'),
+            true
+        )
+        WHERE data_blob ->> 'funding_source' = 'Towns Fund';
+    """)
+
+    # Remove 'funding_source' and 'spend_type' keys from data_blob
+    op.execute("""
+        UPDATE funding
+        SET data_blob = data_blob - '{funding_source, spend_type}';
+    """)
