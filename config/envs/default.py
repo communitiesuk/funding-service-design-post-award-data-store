0c37df313c7092168d6a357d52898b6ec9ebfce8
Samuel Williams <samuelhwilliams@gmail.com>
2024-05-17 11:04:34 +0100

    Copy find into data-store

--- config/envs/default.py
+++ config/envs/default.py
@@ -1,3 +1,4 @@
+import base64
 import os
 from os import environ
 from pathlib import Path
@@ -17,3 +18,21 @@ class DefaultConfig(object):
     AWS_REGION = os.getenv("AWS_REGION")
     AWS_S3_BUCKET_FAILED_FILES = os.getenv("AWS_S3_BUCKET_FAILED_FILES")
     AWS_S3_BUCKET_SUCCESSFUL_FILES = os.getenv("AWS_S3_BUCKET_SUCCESSFUL_FILES")
+
+    # ------------- Find config --------------
+    DATA_STORE_HOST = "http://localhost:8080"
+    CONTACT_EMAIL = os.environ.get("CONTACT_EMAIL", "FSD.Support@levellingup.gov.uk")
+    CONTACT_PHONE = os.environ.get("CONTACT_PHONE", "12345678910")
+
+    # Funding Service Design Post Award
+    FSD_USER_TOKEN_COOKIE_NAME = "fsd_user_token"
+    AUTHENTICATOR_HOST = os.environ.get("AUTHENTICATOR_HOST", "authenticator")
+    COOKIE_DOMAIN = os.environ.get("COOKIE_DOMAIN", None)
+    SECRET_KEY = os.environ.get("SECRET_KEY", "dev")
+
+    # RSA 256 KEYS
+    RSA256_PUBLIC_KEY_BASE64 = os.getenv("RSA256_PUBLIC_KEY_BASE64")
+    if RSA256_PUBLIC_KEY_BASE64:
+        RSA256_PUBLIC_KEY = base64.b64decode(RSA256_PUBLIC_KEY_BASE64).decode()
+
+    AUTO_BUILD_ASSETS = False
--- config/envs/development.py
+++ config/envs/development.py
@@ -1,3 +1,4 @@
+import logging
 from os import getenv
 
 from botocore.config import Config
@@ -13,3 +14,18 @@ class DevelopmentConfig(DefaultConfig):
     AWS_ENDPOINT_OVERRIDE = getenv("AWS_ENDPOINT_OVERRIDE")
     AWS_CONFIG = Config(retries={"max_attempts": 1, "mode": "standard"})
     SQLALCHEMY_ECHO = getenv("SQLALCHEMY_ECHO", "false") in {"true", "yes", "1"}
+
+    # RSA 256 KEYS
+    if not hasattr(DefaultConfig, "RSA256_PUBLIC_KEY"):
+        _test_public_key_path = DefaultConfig.FLASK_ROOT / "tests" / "keys" / "rsa256" / "public.pem"
+        with open(_test_public_key_path, mode="rb") as public_key_file:
+            RSA256_PUBLIC_KEY = public_key_file.read()
+    FSD_LOG_LEVEL = logging.DEBUG
+
+    DEBUG_USER_ON = True
+    DEBUG_USER = {
+        "full_name": "Development User",
+        "email": "dev@communities.gov.uk",
+        "roles": [],
+        "highest_role_map": {},
+    }
