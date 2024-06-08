3b949930a6644339d064166e9365b790320194b4
Samuel Williams <samuelhwilliams@gmail.com>
2024-06-08 11:39:35 +0100

    try a dynamic submission form

--- report/forms.py
+++ report/forms.py
@@ -1,5 +1,9 @@
+import dataclasses
+from typing import Callable, Type
+
+from flask import url_for
 from flask_wtf import FlaskForm
-from govuk_frontend_wtf.wtforms_widgets import GovCharacterCount, GovRadioInput, GovSubmitInput
+from govuk_frontend_wtf.wtforms_widgets import GovCharacterCount, GovRadioInput, GovSubmitInput, GovTextInput
 from wtforms import Field, RadioField, StringField, SubmitField
 
 from core.controllers.partial_submissions import (
@@ -43,8 +47,271 @@ class ProjectOverviewProgressSummary(SubmissionDataForm):
 
 
 class UpcomingCommunicationOpportunities(SubmissionDataForm):
-    upcoming_communication_opportunities = RadioField(
+    do_you_have_any = RadioField(
         "Do you have any upcoming communications opportunities?",
         choices=(("yes", "Yes"), ("no", "No")),
         widget=GovRadioInput(),
     )
+
+
+class CommunicationOpportunityTitle(SubmissionDataForm):
+    title = StringField(
+        "Title of the communication opportunity",
+        widget=GovTextInput(),
+    )
+
+
+class CommunicationOpportunityDetails(SubmissionDataForm):
+    details = StringField(
+        "Tell us in more detail about your upcoming communications",
+        widget=GovCharacterCount(),
+    )
+
+
+class CommunicationOpportunityAddAnother(SubmissionDataForm):
+    add_another = RadioField(
+        "Do you want to add any further communications?",
+        choices=(("yes", "Yes"), ("no", "No")),
+        widget=GovRadioInput(),
+    )
+
+
+@dataclasses.dataclass
+class SubmissionForm:
+    path_fragment: str
+    form_class: Type[FlaskForm]
+    template: str
+
+
+@dataclasses.dataclass
+class RepeatableSubmissionForm:
+    do_you_need_form: SubmissionForm | None
+    details_forms: list[SubmissionForm]
+    add_another_form: SubmissionForm
+
+    go_to_details_if: Callable
+    add_another_if: Callable
+
+    max_repetitions: int = 5
+
+    def get_redirect_url_on_submit(self, form):
+        pass
+
+
+@dataclasses.dataclass
+class SubmissionSubsection:
+    name: str
+    path_fragment: str
+    forms: list[SubmissionForm | RepeatableSubmissionForm]
+
+    def find_form(self, path_fragment, number: int | None) -> SubmissionForm:
+        """Find a form by its URL path fragment."""
+        try:
+            for form in self.forms:
+                if isinstance(form, SubmissionForm) and number is None and form.path_fragment == path_fragment:
+                    return form
+
+                elif isinstance(form, RepeatableSubmissionForm):
+                    if form.do_you_need_form and form.do_you_need_form.path_fragment == path_fragment:
+                        return form.do_you_need_form
+
+                    if number is None:
+                        continue
+
+                    if number > form.max_repetitions:
+                        raise KeyError(f"Cannot reach repetition {number} on {form=} - {form.max_repetitions=}")
+
+                    if form.add_another_form.path_fragment == path_fragment:
+                        return form.add_another_form
+
+                    for submission_form in form.details_forms:
+                        if submission_form.path_fragment == path_fragment:
+                            return submission_form
+
+                else:
+                    raise RuntimeError(f"unknown {type(form)=} and {number=} combination")
+
+        except StopIteration as e:
+            raise KeyError(f"no form found with {path_fragment=}") from e
+
+    def get_first_submission_form(self) -> SubmissionForm:
+        if len(self.forms) < 1:
+            raise ValueError(f"no forms added to {self=}")
+
+        if isinstance(self.forms[0], RepeatableSubmissionForm):
+            if self.forms[0].do_you_need_form:
+                return self.forms[0].do_you_need_form
+
+            return self.forms[0].details_forms[0]
+
+        if isinstance(self.forms[0], SubmissionForm):
+            return self.forms[0]
+
+        raise ValueError(f"unknown form type: {type(self.forms[0])=}")
+
+    def get_next_form(self, flask_form, form_number) -> tuple[SubmissionForm, int | None] | tuple[None, None]:  # noqa: C901
+        """OK GROSS NEED TO COME UP WITH A CLEANER INTERFACE HERE
+
+        All this does is find the next SubmissionForm instance based on a FlaskForm and a form number (in the case of
+        a RepeatableSubmissionForm"""
+        # TODO: fix this whole shit
+        return_form_from_next_loop = False
+
+        for form in self.forms:
+            if return_form_from_next_loop:
+                if isinstance(form, SubmissionForm):
+                    return form, None
+                elif isinstance(form, RepeatableSubmissionForm):
+                    if form.do_you_need_form:
+                        return form.do_you_need_form, None
+
+                    return form.details_forms[0], 1
+
+                raise RuntimeError("did not expect to get here")
+
+            if isinstance(form, SubmissionForm) and form_number is None and isinstance(flask_form, form.form_class):
+                return_form_from_next_loop = True
+                continue
+
+            elif isinstance(form, RepeatableSubmissionForm):
+                if (
+                    form.do_you_need_form
+                    and isinstance(flask_form, form.do_you_need_form.form_class)
+                    and form.go_to_details_if(flask_form)
+                ):
+                    return form.details_forms[0], 1
+
+                if form_number is None:
+                    continue
+
+                if form_number > form.max_repetitions:
+                    raise KeyError(f"Cannot reach repetition {form_number} on {form=} - {form.max_repetitions=}")
+
+                if (
+                    form.add_another_form
+                    and isinstance(flask_form, form.add_another_form.form_class)
+                    and form.add_another_if(flask_form)
+                ):
+                    return form.details_forms[0], form_number + 1
+
+                for i, submission_form in enumerate(form.details_forms):
+                    if isinstance(flask_form, submission_form.form_class):
+                        if i + 1 < len(form.details_forms):
+                            return form.details_forms[i + 1], form_number
+
+                        if form_number + 1 > form.max_repetitions:
+                            return_form_from_next_loop = True
+                            continue
+
+                        return form.add_another_form, form_number
+
+            else:
+                raise RuntimeError(f"unknown {type(form)=} and {form_number=} combination")
+
+        return None, None
+
+
+@dataclasses.dataclass
+class SubmissionSection:
+    name: str
+    path_fragment: str
+    subsections: list[SubmissionSubsection]
+
+    def find_subsection(self, path_fragment) -> SubmissionSubsection:
+        try:
+            return next(subsection for subsection in self.subsections if subsection.path_fragment == path_fragment)
+        except StopIteration as e:
+            raise KeyError(f"so subsection found with {path_fragment=}") from e
+
+
+@dataclasses.dataclass
+class FundSubmissionStructure:
+    sections: list[SubmissionSection]
+
+    def find_section(self, path_fragment) -> SubmissionSection:
+        try:
+            return next(section for section in self.sections if section.path_fragment == path_fragment)
+        except StopIteration as e:
+            raise KeyError(f"no section found with {path_fragment=}") from e
+
+    def resolve(
+        self, section_path, subsection_path, form_path, form_number
+    ) -> tuple[SubmissionSection, SubmissionSubsection, SubmissionForm]:
+        section = self.find_section(section_path)
+        subsection = section.find_subsection(subsection_path)
+        form = subsection.find_form(form_path, form_number)
+
+        return section, subsection, form
+
+    @staticmethod
+    def get_first_url_for_section(
+        programme_id, project_id, section: SubmissionSection, subsection: SubmissionSubsection
+    ):
+        return url_for(
+            "report.do_submission_form",
+            programme_id=programme_id,
+            project_id=project_id,
+            section_path=section.path_fragment,
+            subsection_path=subsection.path_fragment,
+            form_path=subsection.get_first_submission_form().path_fragment,
+            number=1 if isinstance(subsection.get_first_submission_form(), RepeatableSubmissionForm) else None,
+        )
+
+    def get_next_form_url(self, *, programme, project, section_path, subsection_path, form_path, form_number):
+        pass
+
+
+submission_structure = FundSubmissionStructure(
+    sections=[
+        SubmissionSection(
+            name="Project overview",
+            path_fragment="project-overview",
+            subsections=[
+                SubmissionSubsection(
+                    name="Progress summary",
+                    path_fragment="progress-summary",  # meh this is a bit duplicative for single-form subsections
+                    forms=[
+                        SubmissionForm(
+                            path_fragment="progress-summary",
+                            form_class=ProjectOverviewProgressSummary,
+                            template="report/project-overview/progress-summary/progress-summary.html",
+                        )
+                    ],
+                ),
+                SubmissionSubsection(
+                    name="Upcoming communications",
+                    path_fragment="upcoming-communications",
+                    forms=[
+                        RepeatableSubmissionForm(
+                            do_you_need_form=SubmissionForm(
+                                path_fragment="do-you-have-any",
+                                form_class=UpcomingCommunicationOpportunities,
+                                template="report/project-overview/upcoming-communications/do-you-have-any.html",
+                            ),
+                            go_to_details_if=lambda form: form.do_you_have_any.data == "yes",
+                            details_forms=[
+                                SubmissionForm(
+                                    path_fragment="title",
+                                    form_class=CommunicationOpportunityTitle,
+                                    template="report/project-overview/upcoming-communications/title.html",
+                                ),
+                                SubmissionForm(
+                                    path_fragment="details",
+                                    form_class=CommunicationOpportunityDetails,
+                                    template="report/project-overview/upcoming-communications/details.html",
+                                ),
+                            ],
+                            add_another_form=SubmissionForm(
+                                path_fragment="add-another",
+                                form_class=CommunicationOpportunityAddAnother,
+                                template="report/project-overview/upcoming-communications/add-another.html",
+                            ),
+                            add_another_if=lambda form: form.add_another.data == "yes",
+                            max_repetitions=5,
+                        ),
+                    ],
+                ),
+            ],
+        ),
+    ]
+)
--- report/views.py
+++ report/views.py
@@ -3,7 +3,7 @@ from fsd_utils.authentication.config import SupportedApp
 from fsd_utils.authentication.decorators import login_required
 
 from core.db.entities import Organisation, Programme, ProjectRef
-from report.forms import ProjectOverviewProgressSummary, UpcomingCommunicationOpportunities
+from report.forms import ProjectOverviewProgressSummary, submission_structure
 from submit.main.decorators import set_user_access
 
 report_blueprint = Blueprint("report", __name__)
@@ -43,6 +43,69 @@ def project_reporting_home(programme_id, project_id):
     return render_template("report/project-reporting-home.html", programme=programme, project=project_ref)
 
 
+@report_blueprint.route("/programme/<programme_id>/project/<project_id>/dynamic-home", methods=["GET"])
+@login_required(return_app=SupportedApp.POST_AWARD_SUBMIT)
+@set_user_access
+def project_dynamic_reporting_home(programme_id, project_id):
+    # Add authorisation checks here.
+    programme = Programme.query.get(programme_id)
+    project_ref = ProjectRef.query.get(project_id)
+    return render_template(
+        "report/project-dynamic-reporting-home.html",
+        programme=programme,
+        project=project_ref,
+        submission_structure=submission_structure,
+    )
+
+
+@report_blueprint.route(
+    "/programme/<programme_id>/project/<project_id>/<section_path>/<subsection_path>/<form_path>",
+    methods=["GET", "POST"],
+)
+@report_blueprint.route(
+    "/programme/<programme_id>/project/<project_id>/<section_path>/<subsection_path>/<form_path>/<int:form_number>",
+    methods=["GET", "POST"],
+)
+@login_required(return_app=SupportedApp.POST_AWARD_SUBMIT)
+@set_user_access
+def do_submission_form(
+    programme_id, project_id, section_path, subsection_path, form_path, form_number: int | None = None
+):
+    # Add authorisation checks here.
+    programme = Programme.query.get(programme_id)
+    project_ref = ProjectRef.query.get(project_id)
+
+    submission_section, submission_subsection, submission_form = submission_structure.resolve(
+        section_path, subsection_path, form_path, form_number
+    )
+
+    form = submission_form.form_class.create_and_populate(programme, project_ref)
+    if form.validate_on_submit():
+        # TODO: fix saving data for repeatable forms using form_number
+        form.save_submission_data(programme, project_ref)
+
+        next_form, next_form_number = submission_subsection.get_next_form(form, form_number)
+        if next_form is not None:
+            return redirect(
+                url_for(
+                    "report.do_submission_form",
+                    programme_id=programme.id,
+                    project_id=project_ref.id,
+                    section_path=section_path,
+                    subsection_path=subsection_path,
+                    form_path=next_form.path_fragment,
+                    form_number=next_form_number,
+                )
+            )
+
+        return redirect(
+            url_for("report.project_dynamic_reporting_home", programme_id=programme.id, project_id=project_ref.id)
+        )
+
+    # TODO: fix backlinks, they don't step back to the previous form in the flow
+    return render_template(submission_form.template, programme=programme, project=project_ref, form=form)
+
+
 @report_blueprint.route(
     "/programme/<programme_id>/project/<project_id>/overview/progress-summary", methods=["GET", "POST"]
 )
@@ -60,28 +123,4 @@ def project_overview_progress_summary(programme_id, project_id):
 
     return render_template(
         "report/project-overview-progress-summary.html", programme=programme, project=project_ref, form=form
-    )
-
-
-@report_blueprint.route(
-    "/programme/<programme_id>/project/<project_id>/overview/upcoming-communication-opportunities",
-    methods=["GET", "POST"],
-)
-@login_required(return_app=SupportedApp.POST_AWARD_SUBMIT)
-@set_user_access
-def project_overview_upcoming_communication_opportunities(programme_id, project_id):
-    # Add authorisation checks here.
-    programme = Programme.query.get(programme_id)
-    project_ref = ProjectRef.query.get(project_id)
-
-    form = UpcomingCommunicationOpportunities.create_and_populate(programme, project_ref)
-    if form.validate_on_submit():
-        form.save_submission_data(programme, project_ref)
-        return redirect(url_for("report.project_reporting_home", programme_id=programme_id, project_id=project_id))
-
-    return render_template(
-        "report/project-overview-upcoming-communication-opportunities.html",
-        programme=programme,
-        project=project_ref,
-        form=form,
     )
