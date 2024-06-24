dd17ea1a6592bbba7b118cc044fe90076d9df263
Samuel Williams <samuelhwilliams@gmail.com>
2024-06-24 13:56:36 +0100

    Remove HTTP concept from `get_failed_submission` function
    This function used to be directly plumbed into HTTP requests. Now, it's
    just going to be an interface that views/controllers can call directly,
    so needs no knowledge of HTTP concerns.

--- tests/controller_tests/test_failed_submission.py
+++ tests/controller_tests/test_failed_submission.py
@@ -5,6 +5,7 @@ import pytest
 from werkzeug.datastructures import FileStorage
 
 from core.const import EXCEL_MIMETYPE
+from core.controllers.failed_submission import get_failed_submission
 
 
 @pytest.fixture(scope="module")
@@ -12,35 +13,29 @@ def mocked_failed_submission() -> FileStorage:
     return FileStorage(stream=BytesIO(b"some bytes"), filename="mocked_file.xlsx", content_type=EXCEL_MIMETYPE)
 
 
-@pytest.mark.xfail
 def test_get_failed_submission_success(test_session, mocker, mocked_failed_submission):
     mocker.patch("core.controllers.failed_submission.get_failed_file", return_value=mocked_failed_submission)
 
     valid_uuid = uuid.uuid4()
-    endpoint = f"/failed-submission/{valid_uuid}"
-    response = test_session.get(endpoint)
+    file = get_failed_submission(str(valid_uuid))
 
-    assert response.status_code == 200
-    assert response.data
+    assert file.filename == "mocked_file.xlsx"
+    assert file.content_type == EXCEL_MIMETYPE
 
 
-@pytest.mark.xfail
 def test_get_failed_submission_failure_invalid_id(test_session, mocked_failed_submission):
     invalid_uuid = "12345"
-    endpoint = f"/failed-submission/{invalid_uuid}"
-    response = test_session.get(endpoint)
+    with pytest.raises(ValueError) as e:
+        get_failed_submission(invalid_uuid)
 
-    assert response.status_code == 400
-    assert response.json["detail"] == "Bad Request: failure_uuid is not a valid UUID."
+    assert str(e.value) == "failure_uuid is not a valid UUID."
 
 
-@pytest.mark.xfail
 def test_get_failed_submission_failure_file_not_found(test_session, mocker, mocked_failed_submission):
     mocker.patch("core.controllers.failed_submission.get_failed_file", return_value=None)
 
     valid_uuid = uuid.uuid4()
-    endpoint = f"/failed-submission/{valid_uuid}"
-    response = test_session.get(endpoint)
+    with pytest.raises(FileNotFoundError) as e:
+        get_failed_submission(str(valid_uuid))
 
-    assert response.status_code == 404
-    assert response.json["detail"] == f"File not found: id={valid_uuid} does not match any stored failed files."
+    assert str(e.value) == f"File not found: id={valid_uuid} does not match any stored failed files."
