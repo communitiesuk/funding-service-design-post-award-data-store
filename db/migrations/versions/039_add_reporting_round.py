914fec681e65f3034724e3895ab43ef974e8211e
jd-mhclg <jordan.dale@communities.gov.uk>
2024-07-31 16:34:25 +0100

    Updating Migration 039 to populate reporting_round_entity

--- db/migrations/versions/039_add_reporting_round.py
+++ db/migrations/versions/039_add_reporting_round.py
@@ -54,6 +54,38 @@ def upgrade():
     op.add_column("submission_dim", sa.Column("reporting_round_id", data_store.db.types.GUID(), nullable=True))
     op.create_foreign_key(None, "submission_dim", "reporting_round", ["reporting_round_id"], ["id"])
 
+    # Populate reporting_round with exisitng table data
+    op.execute("""
+    INSERT INTO reporting_round(id,round_number,fund_id,observation_period_start,
+	    observation_period_end,submission_window_start,submission_window_end)
+    SELECT
+		gen_random_uuid() AS id,
+		pj.reporting_round AS round_number,
+		fd.id AS fund_id,
+		sd.reporting_period_start AS observation_period_start, 
+		sd.reporting_period_end AS observation_period_end,
+		NULL AS submission_window_start,
+		NULL AS submission_window_end
+    FROM public.submission_dim sd
+    JOIN public.programme_junction pj ON sd.id = pj.submission_id
+    JOIN public.programme_dim pd ON pj.programme_id = pd.id
+    JOIN public.fund_dim fd ON pd.fund_type_id = fd.id
+    GROUP BY 
+	    pj.reporting_round,
+	    fund_id,
+	    observation_period_start,
+	    observation_period_end
+    """)
+    
+    # Update submission_window_end based on previous hard_coded github dates
+    op.execute("""
+    UPDATE reporting_round SET submission_window_end = '2023-12-04' WHERE fund_id ='4a6e9f7d-fc9d-4c12-b1b6-89e784c310e1';
+    UPDATE reporting_round SET submission_window_end = '2024-05-28' WHERE fund_id ='4a6e9f7d-fc9d-4c12-b1b6-89e784c310e1';
+    UPDATE reporting_round SET submission_window_end = '2023-12-04' WHERE fund_id ='9fde58b2-8a89-4b2c-af7d-1f968b03c7b9';
+    UPDATE reporting_round SET submission_window_end = '2024-05-28' WHERE fund_id ='9fde58b2-8a89-4b2c-af7d-1f968b03c7b9';
+    UPDATE reporting_round SET submission_window_end = '2024-04-30' WHERE fund_id ='e8c7c1c8-90d3-4b2d-aa50-4a2d4091d4f3';
+    """)
+
 
 def downgrade():
     # Remove reporting_round_id from Submission
