dcbe42c69759b3c4dbc96f7499239555bfef3d13
Zoltan Szabo <38726653+sc15zs@users.noreply.github.com>
2024-09-02 11:43:34 +0100

    Modify risk_register update to make that more efficient

--- scripts/anonymise_2.sql
+++ scripts/anonymise_2.sql
@@ -1,4 +1,3 @@
-
 CREATE OR REPLACE FUNCTION anonymize_with_lorem_ipsum(
     table_name TEXT,
     column_name TEXT,
@@ -23,9 +22,6 @@ BEGIN
         'UPDATE %I SET %I = jsonb_set(%I, %L, to_jsonb(anon.lorem_ipsum(characters := length(%I->>%L)))) WHERE %I->>%L IS NOT NULL',
         table_name, column_name, column_name, format('{%s}', data_blob_key), column_name, data_blob_key, column_name, data_blob_key
     );
-
-    -- RAISE NOTICE 'Executing query: %', query;
-
     EXECUTE query;
 
     GET DIAGNOSTICS row_count = ROW_COUNT;
@@ -118,6 +114,13 @@ DECLARE
     pre_update_count INTEGER;
 BEGIN
     RAISE NOTICE 'Anonymising table: %, column_name: %, with random region in the UK.', UPPER(table_name), UPPER(column_name);
+
+    count_query := format(
+        'SELECT COUNT(*) FROM %I WHERE %I IS NOT NULL',
+        table_name, column_name
+    );
+    EXECUTE count_query INTO pre_update_count;
+    RAISE NOTICE 'Number of rows to be updated: %', pre_update_count;
 
     query := format(
         'UPDATE %I SET %I = anon.fake_city_in_country(''United Kingdom'') || '' Council'' WHERE %I IS NOT NULL',
@@ -256,86 +259,35 @@ CREATE OR REPLACE FUNCTION anonymize_risk_register_table() RETURNS VOID AS $$
 DECLARE
     row_count INTEGER;
     pre_update_count INTEGER;
+    lorem_text TEXT;
 BEGIN
     RAISE NOTICE 'Anonymising multiple DATA_BLOB keys in RISK_REGISTER table';
 
     SELECT COUNT(*) INTO pre_update_count FROM risk_register WHERE data_blob IS NOT NULL;
     RAISE NOTICE 'Number of rows to be updated: %', pre_update_count;
 
+    lorem_text := anon.lorem_ipsum(words := 30);
+
     BEGIN
         UPDATE risk_register
         SET data_blob = jsonb_set(
-            jsonb_set(
-                jsonb_set(
-                    jsonb_set(
-                        jsonb_set(
-                            jsonb_set(
-                                data_blob,
-                                '{full_desc}',
-                                to_jsonb(anon.lorem_ipsum(words := 20))
-                            ),
-                            '{risk_name}',
-                            to_jsonb(anon.lorem_ipsum(characters := 20))
-                        ),
-                        '{short_desc}',
-                        to_jsonb(anon.lorem_ipsum(words := 6))
-                    ),
-                    '{mitigations}',
-                    to_jsonb(anon.lorem_ipsum(words := 10))
-                ),
-                '{consequences}',
-                to_jsonb(anon.lorem_ipsum(words := 5))
-            ),
-            '{risk_owner_role}',
-            to_jsonb(anon.lorem_ipsum(words := 3))
+            data_blob,
+            '{}',
+            jsonb_build_object(
+                'full_desc', substring(lorem_text from 1 for 20),
+                'risk_name', substring(lorem_text from 1 for 2),
+                'short_desc', substring(lorem_text from 1 for 6),
+                'mitigations', substring(lorem_text from 1 for 10),
+                'consequences', substring(lorem_text from 1 for 5),
+                'risk_owner_role', substring(lorem_text from 1 for 3)
+            )
         )
         WHERE data_blob IS NOT NULL;
 
         GET DIAGNOSTICS row_count = ROW_COUNT;
-        RAISE NOTICE 'UPDATED % ROWS
-        ', row_count;
+        RAISE NOTICE 'UPDATED % ROWS', row_count;
     EXCEPTION WHEN unique_violation THEN
         RAISE NOTICE 'Unique violation error occurred during risk_register detailed descriptions update. Skipping conflicting values.';
-    END;
-END;
-$$ LANGUAGE plpgsql;
-
-
-
--- PLACE_DETAIL TABLE: Anonymise Name, Email and Telephone in data_blob column
-CREATE OR REPLACE FUNCTION anonymize_place_detail_table() RETURNS VOID AS $$
-DECLARE
-    row_count INTEGER;
-    pre_update_count INTEGER;
-BEGIN
-    RAISE NOTICE 'PLACE_DETAIL table: anonymising Name, Email and Telephone keys with fake data in DATA_BLOB column';
-
-    SELECT COUNT(*) INTO pre_update_count FROM place_detail WHERE data_blob IS NOT NULL;
-    RAISE NOTICE 'Number of rows to be checked: %', pre_update_count;
-
-    BEGIN
-        UPDATE place_detail
-        SET data_blob = jsonb_set(
-            data_blob,
-            '{answer}',
-            COALESCE(
-                CASE
-                    WHEN data_blob->>'indicator' = 'Name' THEN to_jsonb(anon.fake_last_name() || ' ' || anon.fake_last_name())
-                    WHEN data_blob->>'indicator' IN ('Email', 'Contact email') THEN to_jsonb(anon.fake_email())
-                    WHEN data_blob->>'indicator' = 'Telephone' THEN to_jsonb(anon.random_phone('+44'))
-                    ELSE data_blob->'answer'
-                END,
-                data_blob->'answer'
-            )
-        )
-        WHERE data_blob IS NOT NULL
-        AND (data_blob->>'indicator' IN ('Name', 'Email', 'Contact email', 'Telephone'));
-
-        GET DIAGNOSTICS row_count = ROW_COUNT;
-        RAISE NOTICE 'UPDATED % ROWS
-        ', row_count;
-    EXCEPTION WHEN unique_violation THEN
-        RAISE NOTICE 'Unique violation error occurred during place_detail update. Skipping conflicting values.';
     END;
 END;
 $$ LANGUAGE plpgsql;
@@ -349,26 +301,26 @@ DO $$
 BEGIN
     PERFORM anonymize_with_lorem_ipsum('funding'::TEXT, 'data_blob'::TEXT, 'funding_source'::TEXT);   --- TAKES TOO LONG
 
-    PERFORM anonymize_with_lorem_ipsum('funding_comment'::TEXT, 'data_blob'::TEXT, 'comment'::TEXT);
-    PERFORM anonymize_with_lorem_ipsum('private_investment'::TEXT, 'data_blob'::TEXT, 'additional_comments'::TEXT);
-    PERFORM anonymize_with_lorem_ipsum('programme_progress'::TEXT, 'data_blob'::TEXT, 'answer'::TEXT);
+    -- PERFORM anonymize_with_lorem_ipsum('funding_comment'::TEXT, 'data_blob'::TEXT, 'comment'::TEXT);
+    -- PERFORM anonymize_with_lorem_ipsum('private_investment'::TEXT, 'data_blob'::TEXT, 'additional_comments'::TEXT);
+    -- PERFORM anonymize_with_lorem_ipsum('programme_progress'::TEXT, 'data_blob'::TEXT, 'answer'::TEXT);
 
-    PERFORM anonymize_postcode_in_column('project_dim', 'postcodes');
+    -- PERFORM anonymize_postcode_in_column('project_dim', 'postcodes');
 
-    PERFORM anonymize_postcode_in_data_blob('project_dim', 'locations');
+    -- PERFORM anonymize_postcode_in_data_blob('project_dim', 'locations');
 
-    PERFORM anonymize_region('programme_dim', 'programme_name');
-    PERFORM anonymize_region('organisation_dim', 'organisation_name');
-    PERFORM anonymize_region('project_dim', 'project_name');
+    -- PERFORM anonymize_region('programme_dim', 'programme_name');
+    -- PERFORM anonymize_region('organisation_dim', 'organisation_name');
+    -- PERFORM anonymize_region('project_dim', 'project_name');
 
-    PERFORM anonymize_int_value_with_percentage_range('outcome_data', 'amount', -20, 20);
+    -- PERFORM anonymize_int_value_with_percentage_range('outcome_data', 'amount', -20, 20);
 
-    PERFORM anonymize_email_in_column('submission_dim', 'submitting_user_email');
+    -- PERFORM anonymize_email_in_column('submission_dim', 'submitting_user_email');
 
-    PERFORM anonymize_name_in_data_blob('submission_dim', 'sign_off_name');
+    -- PERFORM anonymize_name_in_data_blob('submission_dim', 'sign_off_name');
 
-    PERFORM anonymize_risk_register_table();
+    -- PERFORM anonymize_risk_register_table();
 
-    PERFORM anonymize_place_detail_table();
+    -- PERFORM anonymize_place_detail_table();
 
 END $$;
