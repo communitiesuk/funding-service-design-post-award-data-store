58beb4f8e5e6f9252e8a79daab2bb755408fb1dc
Albert Artur Kolozsvari <albertartk@gmail.com>
2024-09-14 00:42:36 +0100

    Remove data_store/controllers/failed_submission mypy exception

--- data_store/controllers/failed_submission.py
+++ data_store/controllers/failed_submission.py
@@ -12,11 +12,11 @@ def get_failed_submission(failure_uuid: str) -> str:
     :return: the failed submission
     """
     try:
-        failure_uuid = UUID(failure_uuid, version=4)
+        uuid = UUID(failure_uuid, version=4)
     except ValueError as error:
         raise ValueError("failure_uuid is not a valid UUID.") from error
 
-    file_key = get_failed_file_key(failure_uuid)
+    file_key = get_failed_file_key(uuid)
 
     presigned_url = create_presigned_url(
         bucket_name=Config.AWS_S3_BUCKET_FAILED_FILES, file_key=file_key, filename=file_key
