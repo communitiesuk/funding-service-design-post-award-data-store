c09eefca05e9dd92599f6d8fa59ba85651d184a3
William May <wjrm500@gmail.com>
2024-09-20 10:56:39 +0100

    Preparing Pathfinders fund config in advance of round opening

--- config/envs/development.py
+++ config/envs/development.py
@@ -56,4 +56,5 @@ class DevelopmentConfig(DefaultConfig):
     # do not attempt to send confirmation email if development and no key is set
     SEND_CONFIRMATION_EMAILS = True if DefaultConfig.NOTIFY_API_KEY else False
     ENABLE_TF_R5 = True
+    ENABLE_PF_R2 = True
     # -------------- Submit config: end ----------------
