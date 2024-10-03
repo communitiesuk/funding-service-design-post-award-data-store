153456872027eae969e77f154d2cb6c4b5c78c1f
William May <wjrm500@gmail.com>
2024-10-03 10:12:54 +0100

    FPASF-599 - Pre-emptive fix of issue with validating outputs and outcomes during PF R2 ingestion
    In this change we remove the cross-table validation checks on the outputs, outcomes, bespoke outputs and bespoke outcomes tablesfor the PF R2 ingest pipeline. These checks ensure that (A) all outputs and outcomes are associated with an expected intervention theme and unit of measurement, based on the control tables in the hidden worksheet 'Dropdown Values', and (B) all bespoke outputs and outcomes are expected for the local authority submitting the report, based on the control tables in the hidden worksheets 'Bespoke Outputs' and 'Bespoke Outcomes'. They were in place during R1 ingest as 'tamper checks', given that users could in theory manipulate the spreadsheet to override spreadsheet data validation rules. However, for R2 ingest these checks are likely to cause problems, owing to the fact that during spreadsheet creation, data was copied and pasted into the outputs, outcomes, bespoke outputs and bespoke outcomes tables from an unverified source, severing the connection between the control tables in the hidden worksheets and the user-facing worksheets for these tables and meaning that these checks can no longer be safely assumed to pass. Simply removing these checks from R2 is seen as okay because (A) the relevant columns in the spreadsheet tables are now locked, whereas they were not previously, and (B) identifying and hard-coding expected values as per the actual spreadsheets that have already gone out would be labour-intensive and messy.

--- tests/data_store_tests/validation_tests/pathfinders/cross_table_validation_tests/test_ct_validate_r2.py
+++ tests/data_store_tests/validation_tests/pathfinders/cross_table_validation_tests/test_ct_validate_r2.py
@@ -4,11 +4,9 @@ import pytest
 from data_store.messaging import Message
 from data_store.validation.pathfinders.cross_table_validation.ct_validate_r2 import (
     _check_actual_forecast_reporting_period,
-    _check_bespoke_outputs,
     _check_current_underspend,
     _check_intervention_themes_in_pfcs,
     _check_projects,
-    _check_standard_outcomes,
     cross_table_validate,
 )
 
@@ -19,13 +17,27 @@ def test_cross_table_validation_passes(mock_pf_r2_df_dict):
 
 
 def test_cross_table_validation_fails(mock_pf_r2_df_dict):
+    # First error: Invalid project name in "Project progress"
     mock_pf_r2_df_dict["Project progress"].loc[0, "Project name"] = "Invalid Project"
-    mock_pf_r2_df_dict["Outcomes"].loc[0, "Outcome"] = "Invalid Outcome"
-    mock_pf_r2_df_dict["Outcomes"].loc[0, "Unit of measurement"] = "Invalid Unit of Measurement"
-    mock_pf_r2_df_dict["Bespoke outputs"].loc[0, "Output"] = "Invalid Bespoke Output"
-    mock_pf_r2_df_dict["Outputs"].loc[0, "Unit of measurement"] = "Invalid Unit of Measurement"
+
+    # Second error: Invalid project name in "Project location"
+    mock_pf_r2_df_dict["Project location"].loc[0, "Project name"] = "Another Invalid Project"
+
+    # Third error: Missing current underspend when reporting period is not Q4
+    mock_pf_r2_df_dict["Reporting period"].iloc[0, 0] = "Q1 2024/25: Apr 2024 - Jun 2024"
+    mock_pf_r2_df_dict["Current underspend"].iloc[0, 0] = pd.NA
+
+    # Fourth error: Invalid intervention theme in "Project finance changes"
+    mock_pf_r2_df_dict["Project finance changes"].loc[0, "Intervention theme moved from"] = "Invalid Theme"
+
+    # Fifth error: Actual reporting period in the future
+    mock_pf_r2_df_dict["Project finance changes"].loc[0, "Actual, forecast or cancelled"] = "Actual"
+    mock_pf_r2_df_dict["Project finance changes"].loc[0, "Reporting period change takes place"] = (
+        "Q2 2024/25: Jul 2024 - Sep 2024"
+    )
+
     error_messages = cross_table_validate(mock_pf_r2_df_dict)
-    assert error_messages == [
+    expected_error_messages = [
         Message(
             sheet="Progress",
             section="Project progress",
@@ -34,28 +46,35 @@ def test_cross_table_validation_fails(mock_pf_r2_df_dict):
             error_type=None,
         ),
         Message(
-            sheet="Outputs",
-            section="Standard outputs",
-            cell_indexes=("D1",),
-            description="Unit of measurement 'Invalid Unit of Measurement' is not allowed for this output or outcome.",
+            sheet="Project location",
+            section="Project location",
+            cell_indexes=("B1",),
+            description="Project name 'Another Invalid Project' is not allowed for this organisation.",
             error_type=None,
         ),
         Message(
-            sheet="Outcomes",
-            section="Outcomes",
-            cell_indexes=("C1",),
-            description="Standard outcome value 'Invalid Outcome' is not allowed for intervention theme"
-            " 'Enhancing subregional and regional connectivity'.",
+            sheet="Finances",
+            section="Current underspend",
+            cell_indexes=("B1",),
+            description="Current underspend must be filled in if the reporting period is not Q4.",
             error_type=None,
         ),
         Message(
-            sheet="Outputs",
-            section="Bespoke outputs",
-            cell_indexes=("C1",),
-            description="Bespoke output value 'Invalid Bespoke Output' is not allowed for this organisation.",
+            sheet="Finances",
+            section="Project finance changes",
+            cell_indexes=("E1",),
+            description="Intervention theme 'Invalid Theme' is not allowed.",
+            error_type=None,
+        ),
+        Message(
+            sheet="Finances",
+            section="Project finance changes",
+            cell_indexes=("P1",),
+            description="Reporting period must not be in the future if 'Actual, forecast or cancelled' is 'Actual'.",
             error_type=None,
         ),
     ]
+    assert error_messages == expected_error_messages
 
 
 def test__check_projects_passes(mock_pf_r2_df_dict):
@@ -71,43 +90,6 @@ def test__check_projects_fails(mock_pf_r2_df_dict):
             section="Project progress",
             cell_indexes=("B1",),
             description="Project name 'Invalid Project' is not allowed for this organisation.",
-            error_type=None,
-        )
-    ]
-
-
-def test__check_standard_outcomes_passes(mock_pf_r2_df_dict):
-    _check_standard_outcomes(mock_pf_r2_df_dict)
-
-
-def test__check_standard_outcomes_fails(mock_pf_r2_df_dict):
-    mock_pf_r2_df_dict["Outcomes"].loc[0, "Outcome"] = "Invalid Outcome"
-    error_messages = _check_standard_outcomes(mock_pf_r2_df_dict)
-    assert error_messages == [
-        Message(
-            sheet="Outcomes",
-            section="Outcomes",
-            cell_indexes=("C1",),
-            description="Standard outcome value 'Invalid Outcome' is not allowed for intervention theme"
-            " 'Enhancing subregional and regional connectivity'.",
-            error_type=None,
-        )
-    ]
-
-
-def test__check_bespoke_outputs_passes(mock_pf_r2_df_dict):
-    _check_bespoke_outputs(mock_pf_r2_df_dict)
-
-
-def test__check_bespoke_outputs_fails(mock_pf_r2_df_dict):
-    mock_pf_r2_df_dict["Bespoke outputs"].loc[0, "Output"] = "Invalid Bespoke Output"
-    error_messages = _check_bespoke_outputs(mock_pf_r2_df_dict)
-    assert error_messages == [
-        Message(
-            sheet="Outputs",
-            section="Bespoke outputs",
-            cell_indexes=("C1",),
-            description="Bespoke output value 'Invalid Bespoke Output' is not allowed for this organisation.",
             error_type=None,
         )
     ]
