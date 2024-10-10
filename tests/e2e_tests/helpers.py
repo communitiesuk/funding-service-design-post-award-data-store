03328bdc704a6623f27178588236cf5b38b6ff7e
israr-ulhaq <israrawan619@gmail.com>
2024-10-10 14:03:37 +0100

    Fix duplication, add updated test files

--- tests/e2e_tests/helpers.py
+++ tests/e2e_tests/helpers.py
@@ -6,10 +6,13 @@ import time
 
 from click.testing import CliRunner
 from notifications_python_client import NotificationsAPIClient
+from playwright.sync_api import Page, expect
 
 from data_store.cli import set_roles_to_users
 from tests.e2e_tests.config import EndToEndTestSecrets
 from tests.e2e_tests.dataclasses import Account
+from tests.e2e_tests.pages.find import RetrieveSpreadsheetPage
+from tests.e2e_tests.pages.submit import SubmitUploadPage, SubmitUploadResponsePage
 
 
 def generate_email_address(
@@ -78,20 +81,12 @@ def lookup_find_download_link_for_user_in_govuk_notify(
 def lookup_confirmation_emails(
     email_address: str,
     e2e_test_secrets: EndToEndTestSecrets,
-    fund_type: str,
+    la_email_subject: str,
+    fund_email_subject: str,
     retries: int = 30,
     delay: int = 1,
 ) -> tuple[dict, dict]:
     client = NotificationsAPIClient(e2e_test_secrets.NOTIFY_SUBMIT_API_KEY)
-
-    if fund_type == "PF":
-        la_email_subject = "Your Pathfinders data return has been submitted"
-        fund_email_subject = "Record of a Pathfinders submission for Pathfinders Test Council"
-    else:
-        la_email_subject = "Your Towns Fund data return has been submitted"
-        fund_email_subject = (
-            "Record of a Towns Fund submission for Future High Streets Fund Blackfriars – Northern City Centre"
-        )
 
     la_email = None
     fund_email = None
@@ -127,3 +122,41 @@ def extract_email_link(email: dict) -> str:
     pattern = r"https?:\/\/[\w-]+(?:\.[\w-]+)+(?:[\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])?"
 
     return re.findall(pattern, email["body"])[0]
+
+
+def validate_initial_validation_errors(submit_upload_page: SubmitUploadPage, test_file_path: str):
+    # test initial validation error upload
+    submit_upload_initial_error_page: SubmitUploadResponsePage = submit_upload_page.upload_report(test_file_path)
+
+    expect(submit_upload_initial_error_page.get_title()).to_be_visible()
+    expect(submit_upload_initial_error_page.get_subtitle()).to_be_visible()
+
+
+def validate_general_validation_errors(submit_upload_page: SubmitUploadPage, test_file_path: str):
+    # test general validation error upload
+    submit_upload_general_error_page: SubmitUploadResponsePage = submit_upload_page.upload_report(test_file_path)
+
+    expect(submit_upload_general_error_page.get_title()).to_be_visible()
+    expect(submit_upload_general_error_page.get_subtitle()).to_be_visible()
+
+
+def validate_success_files(submit_upload_page: SubmitUploadPage, test_file_path: str):
+    # test successful Upload
+    submit_upload_success_page: SubmitUploadResponsePage = submit_upload_page.upload_report(test_file_path)
+
+    expect(submit_upload_success_page.get_title()).to_be_visible()
+    expect(submit_upload_success_page.get_subtitle()).to_be_visible()
+
+
+def assert_and_download_fund_email_file(fund_email: dict, page: Page):
+    fund_download_link = extract_email_link(fund_email)
+
+    download_page = RetrieveSpreadsheetPage(page)
+    download_page.navigate(fund_download_link)
+
+    filename = download_page.download_file()
+    assert filename.endswith(".xlsx")
+
+    with open(filename, "rb") as f:
+        download_data = f.read()
+        assert len(download_data)
