67228f1133cb455e7548f0bcbc8599cd6f52e6c9
Samuel Williams <samuelhwilliams@gmail.com>
2024-10-10 07:40:57 +0100

    Re-skin the admin action pages
    A bit of threading-the-needle required here to get classes onto the form
    input fields.
    
    `lib.render_form` is part of flask-admin's form rendering pipeline,
    which takes a `form_opts` parameter (normally a FormOpts instance). This
    has an attribute `widget_args` holding a dict of field names to widget
    args.
    
    The widget args we specify here get passed to the govuk-frontend-wtf
    widgets, which take a `params` argument that broadly match GOV.UK
    Frontend's `params` for nunjucks.

--- admin/forms.py
+++ admin/forms.py
@@ -1,17 +1,20 @@
 from flask_wtf import FlaskForm
+from govuk_frontend_wtf.wtforms_widgets import GovTextInput
 from wtforms import StringField
 from wtforms.validators import DataRequired
 
 
 class ReingestAdminForm(FlaskForm):
-    submission_id = StringField("The submission ID to re-ingest", validators=[DataRequired()])
+    submission_id = StringField("The submission ID to re-ingest", widget=GovTextInput(), validators=[DataRequired()])
 
 
 class RetrieveSubmissionAdminForm(FlaskForm):
-    submission_id = StringField("The submission ID to retrieve", validators=[DataRequired()])
+    submission_id = StringField("The submission ID to retrieve", widget=GovTextInput(), validators=[DataRequired()])
 
 
 class RetrieveFailedSubmissionAdminForm(FlaskForm):
     failure_uuid = StringField(
-        "The failure id corresponding to the failed submission to retrieve", validators=[DataRequired()]
+        "The failure id corresponding to the failed submission to retrieve",
+        widget=GovTextInput(),
+        validators=[DataRequired()],
     )
