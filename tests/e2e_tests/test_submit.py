df05ffff1a5eba55b274d3bae2dc8e35cb729c95
Israr Ul Haq <Israrawan619@gmail.com>
2024-10-11 09:53:37 +0100

    Adding end-to-end tests for Towns Fund R6 (#777)
    * adding e2e tests for towns fund round 6
    
    * Fix duplication, add updated test files

--- tests/e2e_tests/test_submit.py
+++ tests/e2e_tests/test_submit.py
@@ -3,22 +3,28 @@ from playwright.sync_api import Page, expect
 
 from tests.e2e_tests.config import EndToEndTestSecrets
 from tests.e2e_tests.helpers import (
-    extract_email_link,
+    assert_and_download_fund_email_file,
     lookup_confirmation_emails,
+    validate_general_validation_errors,
+    validate_initial_validation_errors,
+    validate_success_files,
 )
-from tests.e2e_tests.pages.find import RetrieveSpreadsheetPage
 from tests.e2e_tests.pages.submit import (
     SubmitDashboardPage,
     SubmitUploadPage,
-    SubmitUploadResponsePage,
 )
 
 pytestmark = pytest.mark.e2e
 
 
 @pytest.mark.user_roles(["PF_MONITORING_RETURN_SUBMITTER", "TF_MONITORING_RETURN_SUBMITTER"])
-def test_submit_report(domains, user_auth, page: Page, e2e_test_secrets: EndToEndTestSecrets):
+def test_pathfinders_submit_report(domains, user_auth, page: Page, e2e_test_secrets: EndToEndTestSecrets):
     PATH_TO_TEST_REPORTS = "tests/integration_tests/mock_pf_returns/"
+    INITIAL_VALIDATION_FAILED_FILE = f"{PATH_TO_TEST_REPORTS}PF_Round_2_Initial_Validation_Failures.xlsx"
+    GENERAL_VALIDATION_FAILED_FILE = f"{PATH_TO_TEST_REPORTS}PF_Round_2_General_Validation_Failures.xlsx"
+    SUCCESS_FILE = f"{PATH_TO_TEST_REPORTS}PF_Round_2_Success.xlsx"
+    la_email_subject = "Your Pathfinders data return has been submitted"
+    fund_email_subject = "Record of a Pathfinders submission for Pathfinders Test Council"
 
     dashboard_page = SubmitDashboardPage(page, domain=domains.submit)
     dashboard_page.navigate()
@@ -32,40 +38,43 @@ def test_submit_report(domains, user_auth, page: Page, e2e_test_secrets: EndToEn
 
     submit_upload_page: SubmitUploadPage = dashboard_page.click_fund("Pathfinders")
 
-    # test initial validation error upload
-    submit_upload_initial_error_page: SubmitUploadResponsePage = submit_upload_page.upload_report(
-        f"{PATH_TO_TEST_REPORTS}/PF_Round_2_Initial_Validation_Failures.xlsx"
+    validate_initial_validation_errors(submit_upload_page, INITIAL_VALIDATION_FAILED_FILE)
+    validate_general_validation_errors(submit_upload_page, GENERAL_VALIDATION_FAILED_FILE)
+    validate_success_files(submit_upload_page, SUCCESS_FILE)
+
+    _, fund_email = lookup_confirmation_emails(
+        user_auth.email_address, e2e_test_secrets, la_email_subject, fund_email_subject
     )
 
-    expect(submit_upload_initial_error_page.get_title()).to_be_visible()
-    expect(submit_upload_initial_error_page.get_subtitle()).to_be_visible()
+    assert_and_download_fund_email_file(fund_email, page)
 
-    # test general validation error upload
-    submit_upload_general_error_page: SubmitUploadResponsePage = submit_upload_page.upload_report(
-        f"{PATH_TO_TEST_REPORTS}/PF_Round_2_General_Validation_Failures.xlsx"
+
+@pytest.mark.user_roles(["TF_MONITORING_RETURN_SUBMITTER", "TF_MONITORING_RETURN_SUBMITTER"])
+def test_towns_fund_submit_report(domains, user_auth, page: Page, e2e_test_secrets: EndToEndTestSecrets):
+    PATH_TO_TEST_REPORTS = "tests/integration_tests/mock_tf_returns/"
+    INITIAL_VALIDATION_FAILED_FILE = f"{PATH_TO_TEST_REPORTS}TF_Round_6_Project_Initial_Validation_Failures.xlsx"
+    GENERAL_VALIDATION_FAILED_FILE = f"{PATH_TO_TEST_REPORTS}TF_Round_6_General_Validation_Failures.xlsx"
+    SUCCESS_FILE = f"{PATH_TO_TEST_REPORTS}TF_Round_6_Success.xlsx"
+    la_email_subject = "Your Towns Fund data return has been submitted"
+    fund_email_subject = "Record of a Towns Fund submission for Town Deal Worcester"
+
+    dashboard_page = SubmitDashboardPage(page, domain=domains.submit)
+    dashboard_page.navigate()
+
+    expect(
+        page.get_by_role(
+            "heading",
+            name="Submit monitoring and evaluation data dashboard",
+        )
+    ).to_be_visible()
+
+    submit_upload_page: SubmitUploadPage = dashboard_page.click_fund("Towns Fund")
+    validate_initial_validation_errors(submit_upload_page, INITIAL_VALIDATION_FAILED_FILE)
+    validate_general_validation_errors(submit_upload_page, GENERAL_VALIDATION_FAILED_FILE)
+    validate_success_files(submit_upload_page, SUCCESS_FILE)
+
+    _, fund_email = lookup_confirmation_emails(
+        user_auth.email_address, e2e_test_secrets, la_email_subject, fund_email_subject
     )
 
-    expect(submit_upload_general_error_page.get_title()).to_be_visible()
-    expect(submit_upload_general_error_page.get_subtitle()).to_be_visible()
-
-    # test successful upload
-    submit_upload_success_page: SubmitUploadResponsePage = submit_upload_page.upload_report(
-        f"{PATH_TO_TEST_REPORTS}/PF_Round_2_Success.xlsx"
-    )
-
-    expect(submit_upload_success_page.get_title()).to_be_visible()
-    expect(submit_upload_success_page.get_subtitle()).to_be_visible()
-
-    _, fund_email = lookup_confirmation_emails(user_auth.email_address, e2e_test_secrets)
-
-    fund_download_link = extract_email_link(fund_email)
-
-    download_page = RetrieveSpreadsheetPage(page)
-    download_page.navigate(fund_download_link)
-
-    filename = download_page.download_file()
-    assert filename.endswith(".xlsx")
-
-    with open(filename, "rb") as f:
-        download_data = f.read()
-        assert len(download_data)
+    assert_and_download_fund_email_file(fund_email, page)
