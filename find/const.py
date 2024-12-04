a6584aba9186df411ebb97c3702ab43a5d84757a
William May <wjrm500@gmail.com>
2024-12-04 12:56:44 +0000

    FS-4871 - Share check internal user functionality across apps

--- find/const.py
+++ find/const.py
@@ -4,8 +4,3 @@ from enum import StrEnum
 class MIMETYPE(StrEnum):
     XLSX = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     JSON = "application/json"
-
-
-class InternalDomain(StrEnum):
-    COMMUNITIES = "@communities.gov.uk"
-    TEST_COMMUNITIES = "@test.communities.gov.uk"
