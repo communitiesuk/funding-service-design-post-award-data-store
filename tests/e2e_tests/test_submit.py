50ebe6d44cbe7c63b073dbae84472ecd7093bc2d
israr-ulhaq <israrawan619@gmail.com>
2025-04-15 15:05:36 +0100

    fix e2e test for PF R3

--- tests/e2e_tests/test_submit.py
+++ tests/e2e_tests/test_submit.py
@@ -5,8 +5,6 @@ from tests.e2e_tests.config import EndToEndTestSecrets
 from tests.e2e_tests.helpers import (
     assert_and_download_fund_email_file,
     lookup_confirmation_emails,
-    validate_general_validation_errors,
-    validate_initial_validation_errors,
     validate_success_files,
 )
 from tests.e2e_tests.pages.submit import (
@@ -20,9 +18,7 @@ pytestmark = pytest.mark.e2e
 @pytest.mark.user_roles(["PF_MONITORING_RETURN_SUBMITTER", "TF_MONITORING_RETURN_SUBMITTER"])
 def test_pathfinders_submit_report(domains, user_auth, page: Page, e2e_test_secrets: EndToEndTestSecrets):
     PATH_TO_TEST_REPORTS = "tests/integration_tests/mock_pf_returns/"
-    INITIAL_VALIDATION_FAILED_FILE = f"{PATH_TO_TEST_REPORTS}PF_Round_2_Initial_Validation_Failures.xlsx"
-    GENERAL_VALIDATION_FAILED_FILE = f"{PATH_TO_TEST_REPORTS}PF_Round_2_General_Validation_Failures.xlsx"
-    SUCCESS_FILE = f"{PATH_TO_TEST_REPORTS}PF_Round_2_Success.xlsx"
+    SUCCESS_FILE = f"{PATH_TO_TEST_REPORTS}PF_Round_3_Success.xlsx"
     la_email_subject = "Your Pathfinders data return has been submitted"
     fund_email_subject = "Record of a Pathfinders submission for Pathfinders Bolton Council"
 
@@ -38,8 +34,6 @@ def test_pathfinders_submit_report(domains, user_auth, page: Page, e2e_test_secr
 
     submit_upload_page: SubmitUploadPage = dashboard_page.click_fund("Pathfinders")
 
-    validate_initial_validation_errors(submit_upload_page, INITIAL_VALIDATION_FAILED_FILE)
-    validate_general_validation_errors(submit_upload_page, GENERAL_VALIDATION_FAILED_FILE)
     validate_success_files(submit_upload_page, SUCCESS_FILE)
 
     _, fund_email = lookup_confirmation_emails(
